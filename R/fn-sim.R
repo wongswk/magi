8273050031f35ea082b80cd69c7bcfac11197089
shihao-yang <shihaoyang@g.harvard.edu>
2017-08-02 19:54:06 -0400

    MCMC initialize at random value, flat improper prior

--- R/fn-sim.R
+++ R/fn-sim.R
@@ -7,6 +7,9 @@ matplot(fn.true$time, data.matrix(fn.true[,-3]), type="l", lty=1)
 
 abc = c(0.2, 0.2, 3)
 
+read.fn.sim <- TRUE
+set.seed(123)
+
 fn.true$dVtrue = with(fn.true, abc[3] * (Vtrue - Vtrue^3/3.0 + Rtrue))
 fn.true$dRtrue = with(fn.true, -1.0/abc[3] * (Vtrue - abc[1] + abc[2]*Rtrue))
 
@@ -19,8 +22,12 @@ fn.sim[,1:2] <- fn.sim[,1:2]+rnorm(length(unlist(fn.sim[,1:2])), sd=0.1)
 #                           robs=fn.sim$Rtrue,
 #                           vobs=fn.sim$Vtrue),
 #                 iter=200, chains=5)
-set.seed(123)
+
 fn.sim <- fn.sim[seq(1,nrow(fn.sim), length=41),]
+if(read.fn.sim){
+  fn.sim <- read.csv("data/fn.sim.csv")[,-1]
+}
+
 matplot(fn.sim$time, data.matrix(fn.sim[,-3]), type="l", lty=1)
 
 init <- list(
@@ -87,8 +94,22 @@ vdRpostcurve <- getMeanDerivCurve(x=fn.sim$time, y.mat=gpsmooth_ss$rtrue, dy.mat
 vdVpostcurve <- getMeanDerivCurve(x=fn.sim$time, y.mat=gpsmooth_ss$vtrue, dy.mat=gpsmooth_ss$dvobs, x.new=fn.true$time,
                                sigma.mat = gpsmooth_ss$sigma, phi.mat = gpsmooth_ss$vphi, gamma.mat=gpsmooth_ss$gamma)
 
+id.max <- which.max(gpsmooth_ss$lp__)
+# reporting and plot for true value ----
 
-matplot(fn.true$time, data.matrix(fn.true[,c(2,5)]), type="l", lty=1, col=c(2,1))
+# "best" log-likelihood based on truth. phi vector found by optim with other inputs set at truth
+loglik( data.matrix(fn.true[seq(1,401,length=41),c("Vtrue","Rtrue")]), init$abc, 
+        c(init$vphi, init$rphi), 0.1,  fn.sim[,1:2], r)
+gpsmooth_ss$lp__[id.max]
+gpsmooth_ss$rphi[id.max,]
+gpsmooth_ss$vphi[id.max,]
+gpsmooth_ss$abc[id.max,]
+gpsmooth_ss$sigma[id.max]
+
+pdf("Gaussian Process behavior at true parameter.pdf", width = 8, height = 8)
+
+matplot(fn.true$time, data.matrix(fn.true[,c(2,5)]), type="l", lty=1, col=c(2,1),
+        ylab="R", main="at true parm")
 points(fn.sim$time, fn.sim$Rtrue, col=2)
 matplot(fn.sim$time, t(gpsmooth_ss$rtrue), col="skyblue",add=TRUE, type="p",lty=1, pch=20)
 matplot(fn.true$time, head(t(vdRpostcurve),nrow(fn.true)), col="skyblue",add=TRUE, type="l",lty=1)
@@ -96,12 +117,14 @@ matplot(fn.sim$time, t(gpsmooth_ss$drobs), col="grey",add=TRUE, type="p",lty=1, 
 matplot(fn.true$time, tail(t(vdRpostcurve),nrow(fn.true)), col="grey",add=TRUE, type="l",lty=1)
 
 
-matplot(fn.true$time, data.matrix(fn.true[,c(1,4)]), type="l", lty=1, col=c(2,1))
+matplot(fn.true$time, data.matrix(fn.true[,c(1,4)]), type="l", lty=1, col=c(2,1),
+        ylab="V", main="at true parm")
 points(fn.sim$time, fn.sim$Vtrue, col=2)
 matplot(fn.sim$time, t(gpsmooth_ss$vtrue), col="skyblue",add=TRUE, type="p",lty=1, pch=20)
 matplot(fn.true$time, head(t(vdVpostcurve),nrow(fn.true)), col="skyblue",add=TRUE, type="l",lty=1)
 matplot(fn.sim$time, t(gpsmooth_ss$dvobs), col="grey",add=TRUE, type="p",lty=1, pch=20)
 matplot(fn.true$time, tail(t(vdVpostcurve),nrow(fn.true)), col="grey",add=TRUE, type="l",lty=1)
+dev.off()
 
 
 #### real simulation ####
@@ -110,9 +133,9 @@ gpsmooth <- stan(file="stan/gp-smooth.stan",
                            robs=fn.sim$Rtrue,
                            vobs=fn.sim$Vtrue,
                            time=fn.sim$time),
-                 iter=100, chains=1, init=list(init), warmup = 50)
+                 iter=200, chains=7, warmup = 100, cores=7)
 
-traceplot(gpsmooth)
+
 gpsmooth_ss <- extract(gpsmooth, permuted=TRUE)
 max(gpsmooth_ss$lp__)
 id.max <- which.max(gpsmooth_ss$lp__)
@@ -122,39 +145,11 @@ abline(h=0.1, col=2)
 hist(gpsmooth_ss$sigma, breaks = 50,main="sigma")
 abline(v=0.1, col=2)
 
-plot(gpsmooth_ss$gamma, type="l",main="gamma")
-hist(gpsmooth_ss$gamma, breaks = 50,main="gamma")
-
-
 vdRpostcurve <- getMeanDerivCurve(x=fn.sim$time, y.mat=gpsmooth_ss$rtrue, dy.mat=gpsmooth_ss$drobs, x.new=fn.true$time,
                                   sigma.mat = gpsmooth_ss$sigma, phi.mat = gpsmooth_ss$rphi, gamma.mat=gpsmooth_ss$gamma)
 
 vdVpostcurve <- getMeanDerivCurve(x=fn.sim$time, y.mat=gpsmooth_ss$vtrue, dy.mat=gpsmooth_ss$dvobs, x.new=fn.true$time,
                                   sigma.mat = gpsmooth_ss$sigma, phi.mat = gpsmooth_ss$vphi, gamma.mat=gpsmooth_ss$gamma)
-
-
-matplot(fn.true$time, data.matrix(fn.true[,c(2,5)]), type="l", lty=1, col=c(2,1))
-points(fn.sim$time, fn.sim$Rtrue, col=2)
-matplot(fn.sim$time, t(gpsmooth_ss$rtrue), col="skyblue",add=TRUE, type="p",lty=1, pch=20)
-matplot(fn.true$time, head(t(vdRpostcurve),nrow(fn.true)), col="skyblue",add=TRUE, type="l",lty=1)
-matplot(fn.sim$time, t(gpsmooth_ss$drobs), col="grey",add=TRUE, type="p",lty=1, pch=20)
-matplot(fn.true$time, tail(t(vdRpostcurve),nrow(fn.true)), col="grey",add=TRUE, type="l",lty=1)
-
-
-matplot(fn.true$time, data.matrix(fn.true[,c(1,4)]), type="l", lty=1, col=c(2,1))
-points(fn.sim$time, fn.sim$Vtrue, col=2)
-matplot(fn.sim$time, t(gpsmooth_ss$vtrue), col="skyblue",add=TRUE, type="p",lty=1, pch=20)
-matplot(fn.true$time, head(t(vdVpostcurve),nrow(fn.true)), col="skyblue",add=TRUE, type="l",lty=1)
-matplot(fn.sim$time, t(gpsmooth_ss$dvobs), col="grey",add=TRUE, type="p",lty=1, pch=20)
-matplot(fn.true$time, tail(t(vdVpostcurve),nrow(fn.true)), col="grey",add=TRUE, type="l",lty=1)
-
-
-save(gpsmooth_ss, vdRpostcurve, vdVpostcurve, file="dump.RData")
-
-# "best" log-likelihood based on truth. phi vector found by optim with other inputs set at truth
-loglik( data.matrix(fn.true[seq(1,401,length=41),c("Vtrue","Rtrue")]), init$abc, 
-        c(init$vphi, init$rphi), 0.1,  fn.sim[,1:2], r)
-# around 385.3999
 
 lglik <- lapply(1:length(gpsmooth_ss$lp__), function(it){
   loglik( cbind(gpsmooth_ss$vtrue[it,], gpsmooth_ss$rtrue[it,]), 
@@ -165,6 +160,44 @@ lglik <- lapply(1:length(gpsmooth_ss$lp__), function(it){
           r)
 })
 
+#### reporting and plot for real simulation ####
+
+summary(unlist(lglik))
+summary(gpsmooth_ss$lp__)
+lglik[[id.max]]
+gpsmooth_ss$rphi[id.max,]
+gpsmooth_ss$vphi[id.max,]
+gpsmooth_ss$abc[id.max,]
+gpsmooth_ss$sigma[id.max]
+
+
+pdf("MCMC initialize at random value (flat improper prior).pdf", width = 8, height = 8)
+id.plot <- seq(1,nrow(gpsmooth_ss$abc),length=100)
+id.plot <- unique(as.integer(id.plot))
+
+traceplot(gpsmooth, pars=names(gpsmooth)[1:8])
+
+matplot(fn.true$time, data.matrix(fn.true[,c(2,5)]), type="l", lty=1, col=c(2,1), 
+        ylab="R", main="full posterior")
+points(fn.sim$time, fn.sim$Rtrue, col=2)
+matplot(fn.sim$time, t(gpsmooth_ss$rtrue[id.plot,]), col="skyblue",add=TRUE, type="p",lty=1, pch=20)
+matplot(fn.true$time, head(t(vdRpostcurve[id.plot,]),nrow(fn.true)), col="skyblue",add=TRUE, type="l",lty=1)
+matplot(fn.sim$time, t(gpsmooth_ss$drobs[id.plot,]), col="grey",add=TRUE, type="p",lty=1, pch=20)
+matplot(fn.true$time, tail(t(vdRpostcurve[id.plot,]),nrow(fn.true)), col="grey",add=TRUE, type="l",lty=1)
+
+
+matplot(fn.true$time, data.matrix(fn.true[,c(1,4)]), type="l", lty=1, col=c(2,1),
+        ylab="V", main="full posterior")
+points(fn.sim$time, fn.sim$Vtrue, col=2)
+matplot(fn.sim$time, t(gpsmooth_ss$vtrue[id.plot,]), col="skyblue",add=TRUE, type="p",lty=1, pch=20)
+matplot(fn.true$time, head(t(vdVpostcurve[id.plot,]),nrow(fn.true)), col="skyblue",add=TRUE, type="l",lty=1)
+matplot(fn.sim$time, t(gpsmooth_ss$dvobs[id.plot,]), col="grey",add=TRUE, type="p",lty=1, pch=20)
+matplot(fn.true$time, tail(t(vdVpostcurve[id.plot,]),nrow(fn.true)), col="grey",add=TRUE, type="l",lty=1)
+
+
+
+
+layout(matrix(1:4,2,byrow = TRUE))
 hist(gpsmooth_ss$abc[,1], main="a")
 abline(v=init$abc[1], col=2)
 hist(gpsmooth_ss$abc[,2], main="b")
@@ -175,20 +208,21 @@ abline(v=init$abc[3], col=2)
 hist(gpsmooth_ss$sigma, main="sigma")
 abline(v=init$sigma, col=2)
 
-plot(unlist(lglik), gpsmooth_ss$lp__)
-# around 81.03348
+hist(gpsmooth_ss$rphi[,1], main="phi1_R")
+hist(gpsmooth_ss$rphi[,2], main="phi2_R")
+hist(gpsmooth_ss$vphi[,1], main="phi1_V")
+hist(gpsmooth_ss$vphi[,2], main="phi2_V")
+
+layout(1)
 
 #' discrepency between STAN log posterior and log likelihood
 #' need to implement the model in plain R later
-lglik[[id.max]]
+plot(unlist(lglik), gpsmooth_ss$lp__, xlab="log likelihood from Rscript",
+     ylab="log posterior from STAN")
 
-gpsmooth_ss$rphi[id.max,]
-gpsmooth_ss$vphi[id.max,]
 
-gpsmooth_ss$abc[id.max,]
-gpsmooth_ss$sigma[id.max]
-
-matplot(fn.true$time, data.matrix(fn.true[,c(2,5)]), type="l", lty=1, col=c(2,1))
+matplot(fn.true$time, data.matrix(fn.true[,c(2,5)]), type="l", lty=1, col=c(2,1),
+        ylab="R", main="maximum a posterior")
 points(fn.sim$time, fn.sim$Rtrue, col=2)
 matplot(fn.sim$time, t(gpsmooth_ss$rtrue[id.max,,drop=FALSE]), col="skyblue",add=TRUE, type="p",lty=1, pch=20)
 matplot(fn.true$time, head(t(vdRpostcurve[id.max,,drop=FALSE]),nrow(fn.true)), col="skyblue",add=TRUE, type="l",lty=1)
@@ -196,9 +230,13 @@ matplot(fn.sim$time, t(gpsmooth_ss$drobs[id.max,,drop=FALSE]), col="grey",add=TR
 matplot(fn.true$time, tail(t(vdRpostcurve[id.max,,drop=FALSE]),nrow(fn.true)), col="grey",add=TRUE, type="l",lty=1)
 
 
-matplot(fn.true$time, data.matrix(fn.true[,c(1,4)]), type="l", lty=1, col=c(2,1))
+matplot(fn.true$time, data.matrix(fn.true[,c(1,4)]), type="l", lty=1, col=c(2,1),
+        ylab="V", main="maximum a posterior")
 points(fn.sim$time, fn.sim$Vtrue, col=2)
 matplot(fn.sim$time, t(gpsmooth_ss$vtrue[id.max,,drop=FALSE]), col="skyblue",add=TRUE, type="p",lty=1, pch=20)
 matplot(fn.true$time, head(t(vdVpostcurve[id.max,,drop=FALSE]),nrow(fn.true)), col="skyblue",add=TRUE, type="l",lty=1)
 matplot(fn.sim$time, t(gpsmooth_ss$dvobs[id.max,,drop=FALSE]), col="grey",add=TRUE, type="p",lty=1, pch=20)
 matplot(fn.true$time, tail(t(vdVpostcurve[id.max,,drop=FALSE]),nrow(fn.true)), col="grey",add=TRUE, type="l",lty=1)
+dev.off()
+
+save(gpsmooth_ss, vdRpostcurve, vdVpostcurve, file="dump.RData")
