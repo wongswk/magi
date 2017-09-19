02b81fdcb778a92b8f63e3cb3489dcd10133a804
shihao-yang <shihaoyang@g.harvard.edu>
2017-09-19 12:39:12 -0400

    use HMC on simple example that illustrates high likelihood density but low neighborhood probability

--- diag/similar-issue.R
+++ diag/similar-issue.R
@@ -2,39 +2,61 @@
 #' original sample from a mixture where the peak dominates
 #' then heat up system (rescale likelihood), we can see sampler leaves the peak
 #' and loglik goes down as more samples are drawn
-mixden <- function(x){
+source("../R/helper/basic_hmc.R")
+source("../R/helper/utilities.r")
+
+mixden <- function(x, grad=F){
   # log(0.05*dnorm(x,-1)+0.9*dnorm(x,0)+0.05*dnorm(x,1))
-  pp <- 0.00001
-  log(pp*dnorm(x,0,0.0001)+(1-pp)*dnorm(x,0,1))/0.1
+  sharpsd <- 0.01
+  temperature <- 0.1
+  pp <- 0.01
+  ret <- log(pp*dnorm(x,0,sharpsd)+(1-pp)*dnorm(x,0,1))/temperature
+  if(grad){
+    val <- (pp*dnorm(x,0,sharpsd)+(1-pp)*dnorm(x,0,1))
+    gradient <- pp*dnorm(x,0,sharpsd)*(-x/sharpsd^2) + (1-pp)*dnorm(x,0,1)*(-x/1^2)
+    gradient <- gradient/val/temperature
+    attr(ret, "gradient") <- gradient
+  }
+  ret
 }
 
-mixden.rescale <- function(x){
-  mixden(x)*26/201
+mixden.rescale <- function(x, grad=F){
+  ret <- mixden(x, grad=grad)
+  ret <- ret*26/201
+  if(grad){
+    attr(ret, "gradient") <- attr(ret, "gradient")*26/201
+  }
+  ret
 }
+
+mixden(0.001,T)
+(mixden(0.001+1e-9)-mixden(0.001))/1e-9
+
+mixden.rescale(0.001,T)
+(mixden.rescale(0.001+1e-9)-mixden.rescale(0.001))/1e-9
+
+mixden.rescale(0.1,T)
+(mixden.rescale(0.1+1e-9)-mixden.rescale(0.1))/1e-9
+
 
 plot.function(function(x) exp(mixden(x)), from=-0.2,to=0.2,n=1e4)
 plot.function(function(x) exp(mixden.rescale(x)), from=-3,to=3,n=1e4)
+
 n.iter <- 1e4
 xsam.orig <- 0
 for(t in 2:n.iter){
-  prop <- rnorm(1,sd=0.0001)+xsam.orig[t-1]
-  if(log(runif(1)) < mixden(prop)-mixden(xsam.orig[t-1])){
-    xsam.orig[t] <- prop
-  }else{
-    xsam.orig[t] <- xsam.orig[t-1]
-  }
+  foo <- basic_hmc(mixden, step=runif(1,0.01,0.02), nsteps= 20, initial=xsam.orig[t-1], return.traj = T)
+  xsam.orig[t] <- foo$final
 }
+plot(xsam.orig)
 plot(mixden.rescale(xsam.orig))
 hist(xsam.orig, breaks=20)
 
 xsam.rescale <- 0
 for(t in 2:n.iter){
-  prop <- rnorm(1,sd=1)+xsam.rescale[t-1]
-  if(log(runif(1)) < mixden.rescale(prop)-mixden.rescale(xsam.rescale[t-1])){
-    xsam.rescale[t] <- prop
-  }else{
-    xsam.rescale[t] <- xsam.orig[t-1]
-  }
+  foo <- basic_hmc(mixden.rescale, step=runif(1,0.1,0.2), nsteps= 20, initial=xsam.rescale[t-1], return.traj = T)
+  xsam.rescale[t] <- foo$final
 }
 plot(mixden.rescale(xsam.rescale))
 hist(xsam.rescale, breaks=200)
+
