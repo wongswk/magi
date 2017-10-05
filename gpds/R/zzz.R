254c3cdd31508d5a03c401bb3b183bfc9539b74a
shihao-yang <shihaoyang@g.harvard.edu>
2017-10-05 00:53:10 -0400

    start up message

---
+++ gpds/R/zzz.R
@@ -0,0 +1,21 @@
+#' @useDynLib gpds
+#' @import Rcpp 
+#' @import microbenchmark
+#' @import RcppArmadillo
+NULL
+
+.onAttach <- function( ... )
+{
+  gpdsLib <- dirname(system.file(package = "gpds"))
+  version <- packageDescription("gpds", lib.loc = gpdsLib)$Version
+  BuildDate <- packageDescription("gpds", lib.loc = gpdsLib)$Date
+  
+  foo <- paste("## \n##  gpds (Version ", version, ", Build Date: ", BuildDate, ")\n", 
+               "##  See https://www.overleaf.com/9732586bkctqjywbxny for additional documentation.\n",
+               "##  Please cite software as:\n",
+               "##   our citation\n",
+               "##   goes here\n",
+               "##   Journal of Statistical Software, XX(X): X-XX. \n##\n",
+               sep = "")
+  packageStartupMessage(foo)
+}
