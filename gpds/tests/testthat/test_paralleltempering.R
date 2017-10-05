31fa7fc779fd36d88e3fad0ddae9b635fc57c60e
shihao-yang <shihaoyang@g.harvard.edu>
2017-10-04 22:31:24 -0400

    seg fault fixed by using RcppArmadillo instead

--- gpds/tests/testthat/test_paralleltempering.R
+++ gpds/tests/testthat/test_paralleltempering.R
@@ -1,6 +1,5 @@
 testthat::context("parallel tempering")
 testthat::test_that("parallel tempering runs without error", {
-  # FIXME disabled for now -- segfault for unknown reason
-  # gpds:::paralleltemperingTest1()
-  # gpds:::paralleltemperingTest2()
+  gpds:::paralleltemperingTest1()
+  gpds:::paralleltemperingTest2()
 })
