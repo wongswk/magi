bd6acc7a74b9927d3dce2529f3b43256b5839f96
shihao-yang <shihaoyang@g.harvard.edu>
2017-10-07 00:03:41 -0400

    clean test for phisigllik and sampling

--- gpds/tests/testthat/test_phisigllikC.R
+++ gpds/tests/testthat/test_phisigllikC.R
@@ -1,27 +1,14 @@
-setwd("~/Workspace/DynamicSys/dynamic-systems/test/")
-library(Rcpp)
-sourceCpp("../src/wrapper.cpp")
+library(gpds)
+VRtrue <- read.csv(system.file("testdata/FN.csv", package="gpds"))
 
-VRtrue <- read.csv("../data/FN.csv")
-
-pram.true <- list(
-  abc=c(0.2,0.2,3),
-  rphi=c(0.9486433, 3.2682434),
-  vphi=c(1.9840824, 1.1185157)
+phitrue <- list(
+  compact1 = c(2.618, 6.381, 0.152, 9.636),
+  rbf = c(0.838, 0.307, 0.202, 0.653),
+  matern = c(2.04, 1.313, 0.793, 3.101)
 )
 
 nobs <- 41
-
-
-library(parallel)
-source("../R/visualization.R")
-source("../R/helper/utilities.r")
-source("../R/helper/basic_hmc.R")
-source("../R/HMC-functions.R")
-
-#noise level
 noise <- 0.5
-pram.true$sigma <- noise
 
 fn.true <- VRtrue
 fn.true$time <- seq(0,20,0.05)
@@ -32,85 +19,34 @@ fn.sim <- fn.sim[seq(1,nrow(fn.sim), length=nobs),]
 tvec.nobs <- fn.sim$time
 foo <- outer(tvec.nobs, t(tvec.nobs),'-')[,1,]
 r <- abs(foo)
-r2 <- r^2
 signr <- -sign(foo)
 
-n.iter <- 100  # number of HMC iterations
-phisig <- matrix(NA,n.iter,5)   # phi and sigma
-
-phisig[1,] <- rep(1,5)
-
-##### Reference values (truth)
-bestCovV <- calCov( c( 1.9840824, 1.1185157) )
-bestCovR <- calCov( c( 0.9486433, 3.2682434) )
-logliknoODE.mar( bestCovV, bestCovR, noise, fn.sim[,1:2])
-xc <- phisigllikTest( c(1.9840824, 1.1185157, 0.9486433, 3.2682434, noise), data.matrix(fn.sim[,1:2]), r)
-xr <- phisigllik( c(1.9840824, 1.1185157, 0.9486433, 3.2682434, noise), fn.sim[,1:2], TRUE)
-
-x0 <- c(1.9840824, 1.1185157, 0.9486433, 3.2682434, noise)
-phisigllik(x0, fn.sim[,1:2], TRUE)
-for(i in 1:5){
-  # check derivative
-  x1 = x0
-  x1[i] = x1[i] + 1e-9
-  print(as.numeric((phisigllik(x1, fn.sim[,1:2]) - phisigllik(x0, fn.sim[,1:2]))/1e-9))
+for(kerneltype in c("compact1","rbf","matern")){
+  testpoint <- abs(rnorm(5))
+  xc <- phisigllikC( testpoint, data.matrix(fn.sim[,1:2]), r, kerneltype)
+  xr <- phisigllik( testpoint, fn.sim[,1:2], r, signr, TRUE, kerneltype)
+  
+  test_that(paste("phisigllik value -", kerneltype), {
+    expect_equal(xc$value, as.numeric(xr), tolerance = 1e-4)
+    expect_equal(as.numeric(xc$grad), attr(xr, "grad"), tolerance = 1e-4)
+  })
+  
+  test_that(paste("phisigllik gradient -", kerneltype), {
+    x0 <- c(phitrue[[kerneltype]]*exp(rnorm(4)/10), noise)
+    gradTrue <- phisigllikC(x0, data.matrix(fn.sim[,1:2]), r, kerneltype)$grad
+    gradNum <- c()
+    for(i in 1:5){
+      x1 = x0
+      x1[i] = x1[i] + 1e-9
+      gradNum[i] <- ((phisigllikC(x1, data.matrix(fn.sim[,1:2]), r, kerneltype)$value - 
+                        phisigllikC(x0, data.matrix(fn.sim[,1:2]), r, kerneltype)$value)/1e-9)
+    }
+    expect_equal(gradNum, as.numeric(gradTrue), tolerance = 1e-4)
+  })
+  
+  test_that(paste("phisigSample runs without error -", kerneltype), {
+    phisigSample(data.matrix(fn.sim[,1:2]), r, c(phitrue[[kerneltype]], noise),
+                 rep(0.03,5), 20, F, kerneltype)
+  })
 }
 
-phisigllik( c(-1, -1, -1, -1, -1), fn.sim[,1:2], TRUE)
-phisigllikTest( c(-1, -1, -1, -1, -1), data.matrix(fn.sim[,1:2]), r)
-# need to handle contraint on HMC
-
-testpoint <- abs(rnorm(5))
-xc <- phisigllikTest( testpoint, data.matrix(fn.sim[,1:2]), r)
-xr <- phisigllik( testpoint, fn.sim[,1:2], TRUE)
-xc$value - xr
-xc$grad - attr(xr, "grad")
-
-phisigSample(data.matrix(fn.sim[,1:2]), r, c(1.9840824, 1.1185157, 0.9486433, 3.2682434, noise),
-             rep(0.03,5), 20, F)
-
-
-xthetaSample(data.matrix(fn.sim[,1:2]), bestCovV, bestCovR, noise, 
-             rep(1, nobs*2+3), rep(0.03,nobs*2+3), 1, T)
-
-xthetallik(matrix(1, nrow=nobs, ncol=2), rep(1,3), bestCovV, bestCovR, noise, data.matrix(fn.sim[,1:2]), grad = T)
-
-#### test RBF kernel ####
-testpoint <- abs(rnorm(5))
-xc <- phisigllikTest( testpoint, data.matrix(fn.sim[,1:2]), r, "rbf")
-xr <- phisigllik( testpoint, fn.sim[,1:2], TRUE, "rbf")
-as.numeric(xc$value - xr)
-xc$grad - attr(xr, "grad")
-
-x0 <- c(1.9840824, 1.1185157, 0.9486433, 3.2682434, noise)
-phisigllikTest(x0, data.matrix(fn.sim[,1:2]), r, "rbf")$grad
-for(i in 1:5){
-  # check derivative
-  x1 = x0
-  x1[i] = x1[i] + 1e-9
-  print(as.numeric((phisigllikTest(x1, data.matrix(fn.sim[,1:2]), r, "rbf")$value - 
-                      phisigllikTest(x0, data.matrix(fn.sim[,1:2]), r, "rbf")$value)/1e-9))
-}
-
-phisigSample(data.matrix(fn.sim[,1:2]), r, c(1.9840824, 1.1185157, 0.9486433, 3.2682434, noise),
-             rep(0.03,5), 20, F, "rbf")
-
-#### test compact1 kernel ####
-testpoint <- abs(rnorm(5))
-xc <- phisigllikTest( testpoint, data.matrix(fn.sim[,1:2]), r, "compact1")
-xr <- phisigllik( testpoint, fn.sim[,1:2], TRUE, "compact1")
-as.numeric(xc$value - xr)
-xc$grad - attr(xr, "grad")
-
-x0 <- c(1.9840824, 1.1185157, 0.9486433, 3.2682434, noise)
-phisigllikTest(x0, data.matrix(fn.sim[,1:2]), r, "rbf")$grad
-for(i in 1:5){
-  # check derivative
-  x1 = x0
-  x1[i] = x1[i] + 1e-9
-  print(as.numeric((phisigllikTest(x1, data.matrix(fn.sim[,1:2]), r, "rbf")$value - 
-                      phisigllikTest(x0, data.matrix(fn.sim[,1:2]), r, "rbf")$value)/1e-9))
-}
-
-phisigSample(data.matrix(fn.sim[,1:2]), r, c(1.9840824, 1.1185157, 0.9486433, 3.2682434, noise),
-             rep(0.03,5), 20, F, "rbf")
