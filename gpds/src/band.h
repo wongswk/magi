546ceb3877476be729cd7971ca6e3d23a14badbb
shihao-yang <shihaoyang@g.harvard.edu>
2017-10-08 23:07:06 -0400

    add lesson learned from bandworks branch

--- gpds/src/band.h
+++ gpds/src/band.h
@@ -16,7 +16,7 @@ extern "C" {
   void xthetallikBandC( const double *xtheta, const double *Vmphi, const double *VKinv, const double *VCinv,
                         const double *Rmphi, const double *RKinv, const double *RCinv, const int *bandsize, const int *nn,
                         const double *sigma, const double *yobs, double *ret, double *retgrad);
-
+  // previous .C wrapper doesn't work with Rcpp auto-generated wrapper
 }
 
 // g++ band.cpp -o band.o -lopenblas -llapack -lm -Wall -L/opt/OpenBLAS/lib -I/opt/OpenBLAS/include
