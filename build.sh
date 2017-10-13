c750d9dcddeba67bac3cfd6627708dcdf5f0530d
shihao-yang <shihaoyang@g.harvard.edu>
2017-10-13 11:10:36 -0400

    add build file for portable cross-flatform compile

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ -z "${CPU}" ]]; then
+    export CPU=$(python -c 'import multiprocessing as mp; print(mp.cpu_count())')
+fi
+
+PROJECT=$(pwd)
+BOOST=boost_1_65_1
+
+
+if [ ! -d "include/$BOOST" ]; then
+
+    cd include
+    wget https://downloads.sourceforge.net/project/boost/boost/1.65.1/$BOOST.tar.gz
+    tar xf $BOOST.tar.gz
+    rm $BOOST.tar.gz
+fi
+
+cd $PROJECT
+
+echo "
+PKG_CXX=clang++
+PKG_CXXFLAGS = -std=c++11 -O3 -DNDEBUG -Wall \$(SHLIB_OPENMP_CXXFLAGS)
+PKG_LIBS = \$(SHLIB_OPENMP_CFLAGS) \$(LAPACK_LIBS) \$(BLAS_LIBS) \$(FLIBS) -I$PROJECT/include/$BOOST
+
+" > gpds/src/Makevars
