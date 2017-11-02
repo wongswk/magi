cc53e76b4709c622a84be941f76d60717d5e42e3
shihao-yang <shihaoyang@g.harvard.edu>
2017-11-02 21:13:36 +0800

    script for run on odyssey

--- odyssey/job_array.sh
+++ odyssey/job_array.sh
@@ -2,10 +2,10 @@
 #SBATCH -J DynamicSystem # A single job name for the array
 #SBATCH -n 1 # Number of cores
 #SBATCH -N 1 # All cores on one machine
-#SBATCH -p serial_requeue # Partition
-#SBATCH --mem 2000 # Memory request
+#SBATCH -p kou # Partition
+#SBATCH --mem 1000 # Memory request
 #SBATCH -t 0-06:00 # (D-HH:MM)
 #SBATCH -o /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.out # Standard output
 #SBATCH -e /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.err # Standard error
 
-Rscript HMC-v3-odyssey.R ${SLURM_ARRAY_TASK_ID}
+Rscript R/two-phase-repeated-sample-pluginTruth.R ${SLURM_ARRAY_TASK_ID}
