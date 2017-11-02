fb2b7f83832e01a42f3e1a4c9eda5b487800859b
shihao-yang <shihaoyang@g.harvard.edu>
2017-11-02 15:25:58 +0800

    clean sticky with flat mean, need investigate

--- gpds/tests/testthat/test_code_flat_mean.R
+++ gpds/tests/testthat/test_code_flat_mean.R
@@ -9,11 +9,10 @@ config <- list(
   npostplot = 5,
   loglikflag = "withmean",
   bandsize = 20,
-  hmcSteps = 100,
+  hmcSteps = 200,
   n.iter = 1e4,
   burninRatio = 0.1,
-  stepSizeFactor = 0.1,
-  bandwidthFactor = 0.5
+  stepSizeFactor = 1
 )
 
 VRtrue <- read.csv(system.file("testdata/FN.csv", package="gpds"))
@@ -47,43 +46,18 @@ r.nobs <- abs(foo)
 r2.nobs <- r.nobs^2
 signr.nobs <- -sign(foo)
 
-phisigllikC( c(1.9840824, 1.1185157, 0.9486433, 3.2682434, config$noise), 
-             data.matrix(fn.sim[!is.nan(fn.sim[,1]),1:2]), r.nobs, config$kernel)
-fn <- function(par) -phisigllikC( par, data.matrix(fn.sim[!is.nan(fn.sim[,1]),1:2]), 
-                                  r.nobs, config$kernel)$value
-gr <- function(par) -as.vector(phisigllikC( par, data.matrix(fn.sim[!is.nan(fn.sim[,1]),1:2]), 
-                                            r.nobs, config$kernel)$grad)
-marlikmap <- optim(rep(1,5), fn, gr, method="L-BFGS-B", lower = 0.0001)
-
-factorList <- seq(0, 2, 0.01)[-1]
-fn.sim.obs <- data.matrix(fn.sim[!is.nan(fn.sim[,1]),1:2])
-fn.sim.obs.minusMu <- t(t(fn.sim.obs) - colMeans(fn.sim.obs))
-
-lliksurface <- sapply(factorList, function(f){
-  mypar <- marlikmap$par
-  mypar[2] <- mypar[2]*f
-  mypar[4] <- mypar[4]*f
-  phisigllikC(mypar , fn.sim.obs.minusMu, 
-              r.nobs, config$kernel)$value  
-})
-plot(factorList, lliksurface, type="l")
-abline(v=0.5, col=2)
-
-# marlikmap$par[c(1,3)] <- marlikmap$par[c(1,3)]*3^2
-# marlikmap$par[c(2,4)] <- marlikmap$par[c(2,4)]*config$bandwidthFactor
-cursigma <- marlikmap$par[5]
-
 curCovV <- calCov(marlikmap$par[1:2], r, signr, bandsize=config$bandsize, 
                   kerneltype=config$kernel)
 curCovR <- calCov(marlikmap$par[3:4], r, signr, bandsize=config$bandsize, 
                   kerneltype=config$kernel)
 cursigma <- marlikmap$par[5]
+
 curCovV$mu <- rep(colMeans(fn.sim.obs)[1], length(curCovV$mu))
 curCovR$mu <- rep(colMeans(fn.sim.obs)[2], length(curCovR$mu))
-
-dotmu <- fODE(pram.true$abc, fn.true[,1:2]) # pretend these are the means for derivatives
-curCovV$dotmu <- rep(0, length(curCovV$mu))
-curCovR$dotmu <- rep(0, length(curCovR$mu))
+# 
+# dotmu <- fODE(pram.true$abc, fn.true[,1:2]) # pretend these are the means for derivatives
+# curCovV$dotmu <- rep(0, length(curCovV$mu))
+# curCovR$dotmu <- rep(0, length(curCovR$mu))
 
 nall <- nrow(fn.sim)
 burnin <- as.integer(config$n.iter*config$burninRatio)
@@ -117,10 +91,11 @@ fn.true$dVtrue = with(c(fn.true,pram.true), abc[3] * (Vtrue - Vtrue^3/3.0 + Rtru
 fn.true$dRtrue = with(c(fn.true,pram.true), -1.0/abc[3] * (Vtrue - abc[1] + abc[2]*Rtrue))
 
 fn.sim$time <- fn.sim.all$time
-gpds:::plotPostSamples(paste0("~/Desktop/",
+gpds:::plotPostSamples(paste0("~/Workspace/DynamicSys/results/",
                               config$loglikflag,"-experiment-",config$kernel,"-",config$seed,".pdf"), 
                        fn.true, fn.sim, gpode, pram.true, config)
 
 absCI <- apply(gpode$abc, 2, quantile, probs = c(0.025, 0.5, 0.975))
 absCI <- rbind(absCI, mean=colMeans(gpode$abc))
 absCI <- rbind(absCI, coverage = (absCI["2.5%",] < pram.true$abc &  pram.true$abc < absCI["97.5%",]))
+absCI
