a39cbc51aac53091e2c325fe6bb1ae612bfc5ce0
shihao-yang <shihaoyang@g.harvard.edu>
2017-11-05 11:26:05 +0800

    simplify code, investigate constant mu in phase1

--- R/two-phase-repeated-sample-pluginTruth.R
+++ R/two-phase-repeated-sample-pluginTruth.R
@@ -4,7 +4,7 @@ library(gpds)
 config <- list(
   nobs = 41,
   noise = 0.1,
-  kernel = "matern",
+  kernel = "generalMatern",
   seed = (as.integer(Sys.time())*104729+sample(1e6,1))%%1e9,
   npostplot = 5,
   loglikflag = "withmean",
@@ -17,6 +17,8 @@ config <- list(
   startAtTruth = TRUE,
   useTrueMu = TRUE
 )
+outDir <- "~/Workspace/DynamicSys/results/2017-11-05/withmean_repSample_pluginTruth/"
+system(paste("mkdir -p", outDir))
 
 VRtrue <- read.csv(system.file("testdata/FN.csv", package="gpds"))
 pram.true <- list(
@@ -80,48 +82,26 @@ cursigma <- sqrt(cursigma)
 nall <- nrow(fn.sim)
 numparam <- nall*2+3
 n.iter <- config$n.iter
-stepLow.traj <- xth.formal <- matrix(NA, n.iter, numparam)
 
-xth.formal[1,] <- c(fn.true$Vtrue, fn.true$Rtrue, pram.true$abc)  
-
-
-lliklist <- accepts <- c()
-accepts[1] <- 1
-
+xInit <- c(fn.true$Vtrue, fn.true$Rtrue, pram.true$abc)  
 burnin <- as.integer(n.iter*config$burninRatio)
-stepLow <- rep(0.00035, 2*nall+3)*config$stepSizeFactor
-timenow <- Sys.time()
-for (t in 2:n.iter) {
-  rstep <- runif(length(stepLow), stepLow, 2*stepLow)
-  foo <- xthetaSample(data.matrix(fn.sim[,1:2]), curCovV, curCovR, cursigma, 
-                      xth.formal[t-1,], rstep, config$hmcSteps, F, loglikflag = config$loglikflag)
-  if(any(is.na(foo$final))) stop()
-  xth.formal[t,] <- foo$final
-  accepts[t] <- foo$acc
-  stepLow.traj[t,] <- stepLow
-  
-  if (t < burnin & t > 10) {
-    if (mean(tail(accepts[1:t],100)) > 0.9) {
-      stepLow <- stepLow * 1.005
-    } else if (mean(tail(accepts[1:t],100)) < 0.6) {
-      stepLow <- stepLow * .995
-    }
-    xthsd <- apply(tail(xth.formal[1:t,],n.iter*config$burninRatio*0.1), 2, sd)
-    if(mean(xthsd)>0) stepLow <- 0.01*xthsd/mean(xthsd)*mean(stepLow) + 0.99*stepLow
-  }
-  lliklist[t] <- foo$lpr
-  
-  if(t %% 100 ==0) methods::show(c(t, mean(tail(accepts[1:t],100)), foo$final[(nall*2+1):(nall*2+3)]))
-}
+stepLowInit <- rep(0.00035, 2*nall+3)*config$stepSizeFactor
 
-gpode <- list(abc=xth.formal[-(1:burnin), (nall*2+1):(nall*2+3)],
+singleSampler <- function(xthetaValues, stepSize) 
+  xthetaSample(data.matrix(fn.sim[,1:2]), curCovV, curCovR, cursigma, 
+               xthetaValues, stepSize, config$hmcSteps, F, loglikflag = config$loglikflag)
+chainSamplesOut <- chainSampler(config, xInit, singleSampler, stepLowInit, verbose=TRUE)
+
+
+gpode <- list(abc=chainSamplesOut$xth[-(1:burnin), (nall*2+1):(nall*2+3)],
               sigma=rep(marlikmap$par[5], n.iter-burnin),
               rphi=matrix(marlikmap$par[3:4], ncol=2,nrow=n.iter-burnin,byrow=T),
               vphi=matrix(marlikmap$par[1:2], ncol=2,nrow=n.iter-burnin,byrow=T),
-              rtrue=xth.formal[-(1:burnin), (nall+1):(nall*2)],
-              vtrue=xth.formal[-(1:burnin), 1:nall],
-              lp__=lliklist[-(1:burnin)],
-              lglik=lliklist[-(1:burnin)])
+              rtrue=chainSamplesOut$xth[-(1:burnin), (nall+1):(nall*2)],
+              vtrue=chainSamplesOut$xth[-(1:burnin), 1:nall],
+              lp__=chainSamplesOut$lliklist[-(1:burnin)],
+              lglik=chainSamplesOut$lliklist[-(1:burnin)])
+
 gpode$fode <- sapply(1:length(gpode$lp__), function(t) 
   with(gpode, fODE(abc[t,], cbind(vtrue[t,],rtrue[t,]))), simplify = "array")
 
@@ -131,8 +111,8 @@ fn.true$dRtrue = with(c(fn.true,pram.true), -1.0/abc[3] * (Vtrue - abc[1] + abc[
 fn.sim$time <- fn.sim.all$time
 
 gpds:::plotPostSamples(paste0(
-  "~/Workspace/DynamicSys/results/2017-11-03/withmean_repSample_pluginTruth/",
-  config$loglikflag,"-experiment-",config$kernel,"-",config$seed,".pdf"), 
+  outDir,
+  config$loglikflag,"-trueMu-",config$kernel,"-",config$seed,".pdf"), 
   fn.true, fn.sim, gpode, pram.true, config)
 
 absCI <- apply(gpode$abc, 2, quantile, probs = c(0.025, 0.5, 0.975))
@@ -140,5 +120,5 @@ absCI <- rbind(absCI, mean=colMeans(gpode$abc))
 absCI <- rbind(absCI, coverage = (absCI["2.5%",] < pram.true$abc &  pram.true$abc < absCI["97.5%",]))
 
 saveRDS(absCI, paste0(
-  "~/Workspace/DynamicSys/results/2017-11-03/withmean_repSample_pluginTruth/",
-  config$loglikflag,"-experiment-",config$kernel,"-",config$seed,".rda"))
+  outDir,
+  config$loglikflag,"-trueMu-",config$kernel,"-",config$seed,".rda"))
