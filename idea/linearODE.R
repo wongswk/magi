1818d2412c8f7da395d3f90b7f7218956f29af36
shihao-yang <shihaoyang@g.harvard.edu>
2017-11-09 11:26:03 +0800

    add frequentist inference for linear ODE

--- idea/linearODE.R
+++ idea/linearODE.R
@@ -1,4 +1,7 @@
 library(deSolve)
+
+# set up linear ODE --------------------------------------------------------
+
 parameters <- c(a=1, b=-0.2)
 state <- c(X = 1)
 
@@ -13,6 +16,10 @@ times <- seq(0, 20, by = 0.05)
 out <- ode(y = state, times = times, func = linearModel, parms = parameters)
 plot(out[,"time"], out[, "X"], type="l")
 
+
+# analytical solution for ODE ---------------------------------------------
+
+
 XfunAnalytic <- function(t, par){
   par[1] + par[2] * exp(par[3] * t)
 }
@@ -21,27 +28,36 @@ par.true <- with(as.list(c(state, parameters)), {
   c(-a/b, X+a/b, b)
 })
 
-linearModelAnalytical <- function(state, parameters) {
-  par <- with(as.list(c(state, parameters)), {
-    c(-a/b, X+a/b, b)
-  })
-  function(t){
-    par[1] + par[2] * exp(par[3] * t)
-  }
-}
 
-Xfunc <- linearModelAnalytical(state, parameters)
+Xfunc <- function(t) XfunAnalytic(t, par.true)
 plot.function(Xfunc, 0, 20, col=2, add=TRUE)
 
-tI <- seq(0, 10, 0.5)
+
+# simulate observations --------------------------------------------------
+nobs <- 21
+tI <- seq(0, 10, length=nobs)
 y <- rnorm(length(tI))*0.1 + Xfunc(tI)
 
-plot(tI, y)
+points(tI, y)
+
+# frequentist inference ------------------------------------------------------
 
 mse <- function(par){
   mean((y - XfunAnalytic(tI, par))^2)
 }
 
-frequentist <- optim(par.true, mse, method = "BFGS", hessian = TRUE)
-sigmaSqEst <- frequentist$value
+frequentist <- optim(par.true, mse, method = "BFGS")
+sigmaEst <- sqrt(frequentist$value)
 
+llik <- function(par){
+  sum(dnorm(y, XfunAnalytic(tI, par), sigmaEst, log = TRUE))
+}
+
+frequentist <- optim(frequentist$par, llik, method = "BFGS", hessian = TRUE, 
+                     control = list(fnscale = -1))
+
+parEst <- frequentist$par
+varMat <- solve(-frequentist$hessian)
+sqrt(diag(varMat))
+
+(parEst - par.true)/sqrt(diag(varMat))
