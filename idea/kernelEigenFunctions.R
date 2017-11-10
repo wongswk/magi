b5f3c57470df12a28514044499f22950ae9de5de
shihao-yang <shihaoyang@g.harvard.edu>
2017-11-10 17:44:07 +0800

    study fourier series

--- idea/kernelEigenFunctions.R
+++ idea/kernelEigenFunctions.R
@@ -1,7 +1,7 @@
 # convergence of eigen value and eigen vector ----------------------------------
-maxT <- 10
-tAll <- seq(0, maxT, length=201)
-eigenList <- lapply(10:201, function(ndis){
+maxT <- 5
+tAll <- seq(0, maxT, length=41)
+eigenList <- lapply(10:length(tAll), function(ndis){
   tDis <- seq(0, maxT, length=ndis)
   signedDist <- outer(tDis, tDis, '-')
   gpcov <- calCov(c(1,1), abs(signedDist), -sign(signedDist))
@@ -21,6 +21,132 @@ matplot(eigenFun[,1,], type="l", lty=1, col=mycolor)
 matplot(eigenFun[,2,], type="l", lty=1, col=mycolor)
 matplot(eigenFun[,3,], type="l", lty=1, col=mycolor)
 
-Kfunc <- function(r) calCovMatern(c(1,1), abs(r), NULL, complexity = 0)$C
-plot.function(Kfunc, from = -5, to = 5, n=1e4)
+# fourier series for super-imposed sin curves ----------------------------------
+acq.freq <- 100                    # data acquisition (sample) frequency (Hz)
+time     <- 6                      # measuring time interval (seconds)
+ts       <- seq(0,time-1/acq.freq,1/acq.freq) # vector of sampling time-points (s) 
+f.0 <- 1/time
 
+dc.component <- 1
+component.freqs <- c(3,7,10)        # frequency of signal components (Hz)
+component.delay <- c(0,0,0)         # delay of signal components (radians)
+component.strength <- c(1.5,.5,.75) # strength of signal components
+
+f   <- function(t,w) { 
+  dc.component + 
+    sum( component.strength * sin(component.freqs*w*t + component.delay)) 
+}
+
+plot.function(Vectorize(function(t) f(t, 2*pi*f.0)),from = 0, to = 6)
+
+w <- 2*pi*f.0
+trajectory <- sapply(ts, function(t) f(t,w))
+head(trajectory,n=30)
+points(ts, trajectory)
+X.k <- fft(trajectory)
+
+P <- 1/f.0
+x0 <- 0
+ndis <- length(X.k)
+fourierFft <- X.k / ndis
+
+Kreconstruct2 <- function(ts) Re(sapply(ts, function(tj) 
+  sum(fourierFft * exp(1i * (0:(ndis-1)) * 2*pi * (tj-x0) / P))))
+
+plot.function(Kreconstruct2, from = 0, to = 6, add=TRUE, col=3, n=1e3)
+
+plot.frequency.spectrum <- function(X.k, xlimits=c(0,length(X.k))) {
+  plot.data  <- cbind(0:(length(X.k)-1), Mod(X.k))
+  
+  # TODO: why this scaling is necessary?
+  plot.data[2:length(X.k),2] <- 2*plot.data[2:length(X.k),2] 
+  
+  plot(plot.data, t="h", lwd=2, main="", 
+       xlab="Frequency (Hz)", ylab="Strength", 
+       xlim=xlimits, ylim=c(0,max(Mod(plot.data[,2]))))
+}
+
+plot.frequency.spectrum(fourierFft, xlimits=c(0,20))
+
+get.trajectory <- function(X.k,ts,acq.freq) {
+  
+  N   <- length(ts)
+  x.n <- rep(0,N)           # create vector to keep the trajectory
+  ks  <- 0:(length(X.k)-1)
+  
+  for(n in 0:(N-1)) {       # compute each time point x_n based on freqs X.k
+    x.n[n+1] <- sum(X.k * exp(1i*2*pi*ks*n/N)) / N
+  }
+  
+  x.n * acq.freq 
+}
+
+x.n <- get.trajectory(X.k,ts,acq.freq) / acq.freq  # TODO: why the scaling?
+# this is simply discrete inverse fourier transform that I already know
+plot(ts,x.n, type="l"); abline(h=0,lty=3)
+lines(ts,trajectory,col="red") # compare with original
+
+# fourier series for sawtooth wave ------------------------------------------
+Kfunc <- function(r) r/pi # defined on [-pi, pi]
+P <- 2*pi
+x0 <- -pi
+maxT <- pi
+plot.function(Kfunc, from = -maxT, to = maxT, n=1e4)
+
+ndis <- 40
+tdis <- seq(-maxT, maxT, length=ndis+1)[-(ndis+1)]
+
+xvals <- Kfunc(tdis)
+fourierFft <- fft(xvals)/ndis
+
+testthat::expect_equal(Re(sapply(0:(ndis-1), function(n)
+  sum(fourierFft * exp(1i * 2*pi * 0:(ndis-1) * n / ndis)))), xvals)
+
+testthat::expect_equal(Re(sapply(tdis, function(tj) 
+  sum(fourierFft * exp(1i * (0:(ndis-1)) * 2*pi * (tj-x0) / P)))), xvals)
+
+fourierCoef <- list(c0 = fourierFft[1])
+fourierCoef$cPos <- 0.5 * fourierFft[-1] * exp(-1i*2*pi*(1:(ndis-1))*x0/P)
+fourierCoef$cNeg <- Conj(fourierCoef$cPos)
+
+Kreconstruct <- function(ti) {
+  Re(fourierCoef$c0 + sum(fourierCoef$cPos * exp(1i * 2*pi * (1:length(fourierCoef$cPos)) * ti / P)) +
+    sum(fourierCoef$cNeg * exp(-1i * 2*pi * (1:length(fourierCoef$cNeg)) * ti / P)))
+}
+Kreconstruct <- Vectorize(Kreconstruct)
+
+Kreconstruct2 <- function(ts) Re(sapply(ts, function(tj) 
+  sum(fourierFft * exp(1i * (0:(ndis-1)) * 2*pi * (tj-x0) / P))))
+
+testthat::expect_equal(Kreconstruct(tdis), xvals)
+testthat::expect_equal(Kreconstruct2(tdis), xvals)
+
+fourierCoef <- list(
+  c0 = 0,
+  cPos = -0.5*1i*2*(-1)^(2:ndis)/(pi*(1:(ndis-1))),
+  cNeg = 0.5*1i*2*(-1)^(2:ndis)/(pi*(1:(ndis-1)))
+)
+
+plot.function(Kreconstruct, from = -maxT, to = maxT, n=1e4, col=2, add=TRUE)
+plot.function(Kreconstruct2, from = -maxT, to = maxT, n=1e4, col=3, add=TRUE)
+# use discrete fft is not going to get back true fourier coefficients
+
+# fourier series for matern kernel ------------------------------------------
+
+Kfunc <- function(r) calCovMatern(c(1,1), abs(r), NULL, complexity = 0)$C
+plot.function(Kfunc, from = 0, to = maxT, n=1e4)
+
+ndis <- 400
+tdis <- seq(0, maxT, length=ndis+1)[-(ndis+1)]
+
+xvals <- Kfunc(tdis)
+
+fourierCoefs <- fft(xvals)/ndis
+
+Kreconstruct <- function(ti) sum(fourierCoefs * exp(1i * (0:(ndis-1)) * 2*pi * ti / maxT))
+Kreconstruct <- Vectorize(Kreconstruct)
+
+points(tdis, xvals, col=3)
+plot.function(Kreconstruct, from = 0, to = maxT, n=1e4, col=2, add=TRUE)
+
+summary(Re(Kreconstruct(tdis)) - xvals)
