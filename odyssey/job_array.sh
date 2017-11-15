c8f0b1e8e7704f803ddbc86fcc161e001b347ea9
shihao-yang <shihaoyang@g.harvard.edu>
2017-11-15 14:19:02 +0800

    more memory allocation for killed jobs

--- odyssey/job_array.sh
+++ odyssey/job_array.sh
@@ -3,7 +3,7 @@
 #SBATCH -n 1 # Number of cores
 #SBATCH -N 1 # All cores on one machine
 #SBATCH -p serial_requeue # Partition
-#SBATCH --mem 500 # Memory request
+#SBATCH --mem 2000 # Memory request
 #SBATCH -t 1-06:00 # (D-HH:MM)
 #SBATCH -o /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.out # Standard output
 #SBATCH -e /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.err # Standard error
