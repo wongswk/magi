bfc71ae00669edfe9504dddc71a42a1746890bac
shihao-yang <shihaoyang@g.harvard.edu>
2017-11-25 21:57:32 -0500

    summary of output, a lot to digest

--- R/summarizeLargeExperiments.R
+++ R/summarizeLargeExperiments.R
@@ -50,3 +50,46 @@ for(arg in 1:length(indicatorArray)){
   sizeSummary[[arg]] <- table(label)
 }
 save.image("largeExperimentSummary.rda")
+# load("../results/2017-11-25/largeExperimentSummary.rda")
+
+length(sizeSummary)
+length(which(sapply(sizeSummary, is.null)))
+length(indicatorArray)
+summary(do.call(rbind, sizeSummary))
+dim(do.call(rbind, sizeSummary))
+
+organizeOutput <- function(maternDf, noise, nobs){
+  indicatorArray <- array(FALSE, dim=c(length(kernel.candidates), 
+                                       length(noise.candidates), 
+                                       length(nobs.candidates),
+                                       length(filllevel.candidates) ))
+  indicatorArray[kernel.candidates == ifelse(maternDf == 2.5, "matern", "generalMatern"), 
+                 noise.candidates == noise, 
+                 nobs.candidates == nobs,
+                 ] <- TRUE
+  indicatorArray[apply(indicatorArray, 1, any),
+                 apply(indicatorArray, 2, any),
+                 apply(indicatorArray, 3, any),
+                 (nobs.candidates[apply(indicatorArray, 3, any)]-1)*2^filllevel.candidates+1 > 801
+                 ] <- FALSE
+  
+  ndiscretize <- (nobs.candidates[apply(indicatorArray, 3, any)]-1)*2^filllevel.candidates+1
+  ndiscretize <- ndiscretize[ndiscretize <= 801]
+  
+  indicatorVec <- which(indicatorArray)
+  outSize <- do.call(rbind, sizeSummary[indicatorVec])
+  outCoverage <- resultSummary[indicatorVec]
+  rownames(outSize) <- names(outCoverage) <- paste0("discretize-", ndiscretize)
+  
+  list(repetitionSize = outSize, coverage = outCoverage)
+}
+
+organizeOutput(maternDf = 2.01, noise = 0.1, nobs = 51)
+
+organizeOutput(maternDf = 2.5, noise = 0.1, nobs = 51)
+
+organizeOutput(maternDf = 2.01, noise = 0.1, nobs = 201)
+
+organizeOutput(maternDf = 2.01, noise = 0.1, nobs = 401) # TODO: re-run for filllevel=0
+
+organizeOutput(maternDf = 2.01, noise = 0.5, nobs = 201)
