7f8608c17f35aab3bcfdf9c3e6938cb0c027352e
shihao-yang <shihaoyang@g.harvard.edu>
2017-11-28 17:43:42 -0500

    extract pdf visualization

--- R/summarizeLargeExperiments.R
+++ R/summarizeLargeExperiments.R
@@ -12,6 +12,7 @@ indicatorArray <- array(FALSE, dim=c(length(kernel.candidates),
 
 resultSummary <- list()
 sizeSummary <- list()
+files2zip <- c()
 
 for(arg in 1:length(indicatorArray)){
   indicatorArray[] <- FALSE
@@ -38,6 +39,7 @@ for(arg in 1:length(indicatorArray)){
                                 "-nobs",nobs,"-noise",noise,"-ndis",ndis,"/"))
   
   allf <- list.files(outDir)
+  allpdff <- head(sort(allf[grep("pdf", allf)]), 9)
   allf <- allf[grep("rds", allf)]
   ci <- sapply(allf, function(f) readRDS(file.path(outDir, f)), 
                simplify = "array")
@@ -48,9 +50,12 @@ for(arg in 1:length(indicatorArray)){
   resultSummary[[arg]] <- sapply(tapply(1:length(label), label, function(id) apply(ci[,,id], 1:2, mean)),
                           identity, simplify = "array")
   sizeSummary[[arg]] <- table(label)
+  files2zip <- c(files2zip, file.path(outDir, allpdff))
 }
 save.image("largeExperimentSummary.rda")
-# load("../results/2017-11-27/largeExperimentSummary.rda")
+cat(files2zip, file="file_list.txt", sep = "\n")
+system("tar -czv -T file_list.txt -f pdfVisuals.tar.gz")
+# load("../results/2017-11-28/largeExperimentSummary.rda")
 
 length(sizeSummary)
 length(which(sapply(sizeSummary, is.null)))
@@ -91,10 +96,9 @@ organizeOutput(maternDf = 2.5, noise = 0.1, nobs = 51)
 
 organizeOutput(maternDf = 2.01, noise = 0.1, nobs = 201)
 
-organizeOutput(maternDf = 2.01, noise = 0.1, nobs = 401) # TODO: re-run for filllevel=0
+organizeOutput(maternDf = 2.01, noise = 0.1, nobs = 401) 
 
 organizeOutput(maternDf = 2.01, noise = 0.5, nobs = 201)
 
 organizeOutput(maternDf = 2.01, noise = 1.0, nobs = 201)
 
-# shiny app?
