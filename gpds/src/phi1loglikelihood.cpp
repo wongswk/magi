20de0c43a89343bbab211b4fa0bdd1e2c7258bbe
shihao-yang <shihaoyang@g.harvard.edu>
2018-06-25 06:52:00 +0800

    compiled for xthetaphi1sigmallik

--- gpds/src/phi1loglikelihood.cpp
+++ gpds/src/phi1loglikelihood.cpp
@@ -3,6 +3,9 @@
 #include "tgtdistr.h"
 #include "hmc.h"
 #include "dynamicalSystemModels.h"
+#include "wrapper.h"
+#include "band.h"
+
 
 using namespace arma;
 
@@ -13,155 +16,176 @@ using namespace arma;
 //' @param yobs        observed data
 lp xthetaphi1sigmallik( const mat & xlatent, 
                         const vec & theta, 
-                        const mat & phi, 
+                        const vec & phi1, 
                         const vec & sigmaInput, 
                         const mat & yobs, 
-                        const vec & xtimes,
-                        const OdeSystem & fOdeModel) {
-  mat distSigned(xtimes.size(), xtimes.size());
-  for(unsigned int i = 0; i < distSigned.n_cols; i++){
-    distSigned.col(i) = xtimes - xtimes(i);
+                        const std::vector<gpcov> & CovAllDimensions,
+                        const OdeSystem & fOdeModel,
+                        const arma::vec & priorTemperatureInput = ones(1),
+                        const bool useBand = false,
+                        const bool useMean = false) {
+  
+  if(useMean){
+    mat xlatentShifted = xlatent;
+    mat yobsShifted = yobs;
+    mat muAllDimension(yobs.n_rows, yobs.n_cols);
+    mat dotmuAllDimension(yobs.n_rows, yobs.n_cols);
+    
+    for(unsigned int i = 0; i < yobs.n_cols; i++){
+      xlatentShifted.col(i) -= CovAllDimensions[i].mu;
+      yobsShifted.col(i) -= CovAllDimensions[i].mu;
+      muAllDimension.col(i) = CovAllDimensions[i].mu;
+      dotmuAllDimension.col(i) = CovAllDimensions[i].dotmu;
+    }
+    
+    OdeSystem fOdeModelShifted = fOdeModel;
+    
+    fOdeModelShifted.fOde = [&muAllDimension, &dotmuAllDimension, &fOdeModel]
+    (const vec & theta, const mat & x) -> mat{
+      return fOdeModel.fOde(theta, x+muAllDimension) - dotmuAllDimension;
+    };
+    
+    fOdeModelShifted.fOdeDx = [&muAllDimension, &dotmuAllDimension, &fOdeModel]
+    (const vec & theta, const mat & x) -> cube{ 
+      return fOdeModel.fOdeDx(theta, x+muAllDimension);
+    };
+    
+    fOdeModelShifted.fOdeDtheta = [&muAllDimension, &dotmuAllDimension, &fOdeModel]
+    (const vec & theta, const mat & x) -> cube{ 
+      return fOdeModel.fOdeDtheta(theta, x+muAllDimension);
+    };
+    
+    return xthetaphi1sigmallik(xlatentShifted, theta, phi1, sigmaInput, yobsShifted, CovAllDimensions, 
+                               fOdeModelShifted, priorTemperatureInput, useBand, false); 
   }
   
-  vector<gpcov> CovAllDimensions(phi.n_cols);
-  for(unsigned int j = 0; j < phi.n_cols; j++){
-    CovAllDimensions[j] = generalMaternCov( phi.col(j), distSigned, 3);
+  int n = yobs.n_rows;
+  int pdimension = yobs.n_cols;
+  
+  lp ret;
+  if (fOdeModel.checkBound(xlatent, theta, &ret)) {
+    return ret;
   }
   
-  vec xtheta = join_vert(vectorise(xlatent), theta);
+  arma::vec priorTemperature(2);
+  if(priorTemperatureInput.n_rows == 1){
+    priorTemperature.fill(as_scalar(priorTemperatureInput));
+  }else if(priorTemperatureInput.n_rows == 2){
+    priorTemperature = priorTemperatureInput;
+  }else{
+    throw std::invalid_argument("priorTemperatureInput must be scaler or 2-vector");
+  }
   
-  vec sigma(phi.n_cols);
+  vec sigma(yobs.n_cols);
   bool sigmaIsScaler = (sigmaInput.size() == 1);
   if (sigmaIsScaler){
     sigma.fill(as_scalar(sigmaInput));
-  }else if(sigmaInput.size() == phi.n_cols){
+  }else if(sigmaInput.size() == yobs.n_cols){
     sigma = sigmaInput;
   }else{
     throw std::runtime_error("sigmaInput dimension not right");
   }
-  
-  lp numerator = xthetallik(xtheta, CovAllDimensions, sigma, yobs, fOdeModel, false, ones(2));
+  vec sigmaSq = square(sigma);
   
   const mat & fderiv = fOdeModel.fOde(theta, xlatent);
-  mat phiGradient(phi.n_rows, phi.n_cols);
-  vec sigmaGradient(sigma.size());
+  const cube & fderivDx = fOdeModel.fOdeDx(theta, xlatent);
+  const cube & fderivDtheta = fOdeModel.fOdeDtheta(theta, xlatent);
   
-  lp bigpost;
-  for(unsigned int pDimEach = 0; pDimEach < yobs.n_cols; pDimEach++){
-    const gpcov & covThisDim = CovAllDimensions[pDimEach];
-    vec eigval(covThisDim.Sigma.n_cols);
-    mat eigvec(covThisDim.Sigma.n_rows, covThisDim.Sigma.n_cols);
-    
-    eig_sym( eigval, eigvec, covThisDim.Sigma );
-    vec eta = eigvec.t() * join_vert(xlatent.col(pDimEach), fderiv.col(pDimEach));
-    
-    bigpost.value += -0.5*covThisDim.Sigma.n_rows*log(2.0*datum::pi) - sum(log(eigval))/2.0 - 0.5*sum(square(eta) / eigval);
-    vec fitLevelError = yobs.col(pDimEach) - xlatent.col(pDimEach);
-    int nobs = uvec(find_finite(yobs.col(pDimEach))).size();
-    fitLevelError(find_nonfinite(fitLevelError)).fill(0.0);
-    bigpost.value += -0.5*log(2.0*datum::pi)*nobs - log(sigma(pDimEach))*nobs - 0.5 * sum(square(fitLevelError) / pow(sigma(pDimEach), 2));
-    
-    vec alpha = eigvec * (eta / eigval);
-    mat facVtemp = alpha * alpha.t() - (eigvec.each_row() % (1.0 / eigval).t()) * eigvec.t();
-    
-    for(unsigned int i=0; i < covThisDim.dSigmadphiCube.n_slices; i++){
-      phiGradient(i, pDimEach) = 0.5*accu(facVtemp % covThisDim.dSigmadphiCube.slice(i));
+  mat res(pdimension, 3);
+  
+  // V 
+  mat fitLevelError = xlatent - yobs;
+  mat fitDerivError(n, pdimension);
+  vec nobs(pdimension);
+  for( int vEachDim = 0; vEachDim < pdimension; vEachDim++){
+    if(useBand){
+      bmatvecmult(CovAllDimensions[vEachDim].mphiBand.memptr(), 
+                  xlatent.colptr(vEachDim), 
+                  &(CovAllDimensions[vEachDim].bandsize), 
+                  &n, 
+                  fitDerivError.colptr(vEachDim));
+      fitDerivError.col(vEachDim) = fderiv.col(vEachDim) - fitDerivError.col(vEachDim);
+    }else{
+      fitDerivError.col(vEachDim) = fderiv.col(vEachDim);
+      fitDerivError.col(vEachDim) -= CovAllDimensions[vEachDim].mphi * xlatent.col(vEachDim); // n^2 operation  
+    }
+    nobs(vEachDim) = uvec(find_finite(yobs.col(vEachDim))).size();
+  }
+  
+  fitLevelError(find_nonfinite(fitLevelError)).fill(0.0);
+  res.col(0) = -0.5 * sum(square( fitLevelError )).t() / sigmaSq - log(sigma) % nobs;
+  vec sigmaGradient = sum(square( fitLevelError )).t() / (sigmaSq % sigma) - nobs / sigma;
+  
+  mat KinvfitDerivError(n, pdimension);
+  mat CinvX(n, pdimension);
+  
+  for( int vEachDim = 0; vEachDim < pdimension; vEachDim++){
+    if(useBand){
+      bmatvecmult(CovAllDimensions[vEachDim].KinvBand.memptr(), 
+                  fitDerivError.colptr(vEachDim), 
+                  &(CovAllDimensions[vEachDim].bandsize), 
+                  &n, 
+                  KinvfitDerivError.colptr(vEachDim));
+      bmatvecmult(CovAllDimensions[vEachDim].CinvBand.memptr(),
+                  xlatent.colptr(vEachDim),  
+                  &(CovAllDimensions[vEachDim].bandsize), 
+                  &n, 
+                  CinvX.colptr(vEachDim));
+    }else{
+      KinvfitDerivError.col(vEachDim) = CovAllDimensions[vEachDim].Kinv * fitDerivError.col(vEachDim);
+      CinvX.col(vEachDim) = CovAllDimensions[vEachDim].Cinv * xlatent.col(vEachDim);  
+    }
+  }
+  res.col(1) = -0.5 * sum(fitDerivError % KinvfitDerivError).t() / phi1 / priorTemperature(0);
+  res.col(2) = -0.5 * sum(xlatent % CinvX).t() / phi1 / priorTemperature(1);
+  
+  // cout << "lglik component = \n" << res << endl;
+  
+  ret.value = accu(res);
+  
+  // cout << "lglik = " << ret.value << endl;
+  
+  // gradient 
+  // V contrib
+  mat eachDimensionC2(n*pdimension+theta.size(), pdimension, fill::zeros);
+  for( int vEachDim = 0; vEachDim < pdimension; vEachDim++){
+    if(useBand){
+      bmatvecmultT(CovAllDimensions[vEachDim].mphiBand.memptr(), 
+                   KinvfitDerivError.colptr(vEachDim), 
+                   &(CovAllDimensions[vEachDim].bandsize), 
+                   &n, 
+                   eachDimensionC2.colptr(vEachDim) + n*vEachDim);
+      // negate
+      eachDimensionC2.col(vEachDim).subvec(n*vEachDim, n*vEachDim+n-1) =
+        -eachDimensionC2.col(vEachDim).subvec(n*vEachDim, n*vEachDim+n-1);
+    }else{
+      eachDimensionC2.col(vEachDim).subvec(n*vEachDim, n*vEachDim+n-1) =
+        -(CovAllDimensions[vEachDim].mphi.t() * KinvfitDerivError.col(vEachDim));  
     }
     
-    sigmaGradient(pDimEach) = sum(square(fitLevelError)) / pow(sigma(pDimEach), 3) - nobs / sigma(pDimEach);
+    eachDimensionC2.col(vEachDim).subvec(0, n*pdimension-1) +=
+      vectorise(fderivDx.slice(vEachDim).each_col() % KinvfitDerivError.col(vEachDim));
+    
+    eachDimensionC2.col(vEachDim).subvec(n*pdimension, eachDimensionC2.n_rows-1) =
+      fderivDtheta.slice(vEachDim).t() * KinvfitDerivError.col(vEachDim);
   }
   
   if(sigmaIsScaler){
-    bigpost.gradient.set_size(numerator.gradient.size() + phiGradient.size() + 1);
-    bigpost.gradient(bigpost.gradient.size()-1) = sum(sigmaGradient);
+    ret.gradient.set_size(n*pdimension + theta.size() + phi1.size() + 1);
+    ret.gradient(ret.gradient.size() - 1) = sum(sigmaGradient);
   }else{
-    bigpost.gradient.set_size(numerator.gradient.size() + phiGradient.size() + sigma.size());
-    bigpost.gradient.subvec(numerator.gradient.size() + phiGradient.size(), bigpost.gradient.size()-1) = sigmaGradient;
-  }
-  bigpost.gradient.subvec(0, numerator.gradient.size()-1) = numerator.gradient;
-  bigpost.gradient.subvec(numerator.gradient.size(), numerator.gradient.size() + phiGradient.size()-1) = vectorise(phiGradient);
-  return bigpost;
-}
-
-//' sample from GP ODE for latent x and theta
-//' @export
-// [[Rcpp::export]]
-Rcpp::List xthetaphi1sigmaSample( const arma::mat & xInitial, 
-                                  const arma::vec & thetaInitial,
-                                  const arma::mat & phiInitial, 
-                                  const arma::vec & sigmaInitial, 
-                                  const arma::mat & yobs, 
-                                  const arma::vec & xtimes,
-                                  const arma::vec & step,
-                                  const std::string modelName = "FN",
-                                  const int nsteps = 1, 
-                                  const bool traj = false){
-  const std::string loglikflag = "usual";
-  
-  OdeSystem model;
-  if(modelName == "FN"){
-    model = OdeSystem(fnmodelODE, fnmodelDx, fnmodelDtheta, zeros(3), ones(3)*datum::inf);
-  }else if(modelName == "Hes1"){
-    model = OdeSystem(hes1modelODE, hes1modelDx, hes1modelDtheta, zeros(7), ones(7)*datum::inf); 
-  }else if(modelName == "HIV"){
-    model = OdeSystem(HIVmodelODE, HIVmodelDx, HIVmodelDtheta, {-datum::inf, 0,0,0,0,0, -datum::inf,-datum::inf,-datum::inf}, ones(9)*datum::inf);   
-  }else{
-    throw std::runtime_error("modelName must be one of 'FN', 'Hes1', 'Hes1-log', 'HIV'");
+    ret.gradient.set_size(n*pdimension + theta.size() + phi1.size() + sigma.size());
+    ret.gradient.subvec(ret.gradient.size() - sigma.size(), ret.gradient.size() - 1) = sigmaGradient;
   }
   
-  std::function<lp(vec)> tgt;
-  if(loglikflag == "usual"){
-    tgt = [&](const vec & xthetaphisigma) -> lp{
-      const mat xlatent = mat(const_cast<double*>(xthetaphisigma.memptr()),
-                              xInitial.n_rows, xInitial.n_cols, false, false);
-      const mat theta = mat(const_cast<double*>(xthetaphisigma.memptr() + xlatent.size()),
-                            thetaInitial.n_rows, thetaInitial.n_cols, false, false);
-      const mat phi = mat(const_cast<double*>(xthetaphisigma.memptr() + xlatent.size() + theta.size()),
-                          phiInitial.n_rows, phiInitial.n_cols, false, false);
-      const mat sigma = mat(const_cast<double*>(xthetaphisigma.memptr() + xlatent.size() + theta.size() + phi.size()),
-                            sigmaInitial.n_rows, sigmaInitial.n_cols, false, false);
-      return xthetaphisigmallik( xlatent, theta, phi, sigma, yobs, xtimes, model);
-    };
-  }else if(loglikflag == "withmean"){
-    throw std::runtime_error("withmean not supported yet");
-  }else if(loglikflag == "band"){
-    throw std::runtime_error("band not supported yet");
-  }else if(loglikflag == "withmeanBand"){
-    throw std::runtime_error("withmeanBand not supported yet");
-  }else{
-    throw std::runtime_error("loglikflag must be 'usual', 'withmean', 'band', or 'withmeanBand'");
-  }
+  ret.gradient.subvec(0, eachDimensionC2.n_rows-1) = -sum(eachDimensionC2.each_row() / phi1.t(), 1) / priorTemperature(0);
+  ret.gradient.subvec(0, n*pdimension-1) -= vectorise(CinvX.each_row() / phi1.t()) / priorTemperature(1);
+  fitLevelError.each_row() /= sigmaSq.t();
+  ret.gradient.subvec(0, n*pdimension-1) -= vectorise(fitLevelError);
   
-  unsigned int nparam = xInitial.size() + thetaInitial.size() + phiInitial.size() + sigmaInitial.size();
-  vec initial(nparam);
+  ret.gradient.subvec(eachDimensionC2.n_rows, eachDimensionC2.n_rows + phi1.size() - 1) = 
+    (-res.col(1) / phi1) + (-res.col(2) / phi1);
   
-  unsigned int assignmentIterator = 0;
-  initial.subvec(assignmentIterator, assignmentIterator + xInitial.size() - 1) = vectorise(xInitial);
-  assignmentIterator += xInitial.size();
-  initial.subvec(assignmentIterator, assignmentIterator + thetaInitial.size() - 1) = vectorise(thetaInitial);
-  assignmentIterator += thetaInitial.size();
-  initial.subvec(assignmentIterator, assignmentIterator + phiInitial.size() - 1) = vectorise(phiInitial);
-  assignmentIterator += phiInitial.size();
-  initial.subvec(assignmentIterator, assignmentIterator + sigmaInitial.size() - 1) = vectorise(sigmaInitial);
-  
-  vec lb = ones<vec>(nparam) * (-datum::inf);
-  lb.subvec(xInitial.size(), xInitial.size() + thetaInitial.size() - 1) = model.thetaLowerBound;
-  lb.subvec(xInitial.size() + thetaInitial.size(), lb.size() - 1).fill(1e-2);
-  
-  hmcstate post = basic_hmcC(tgt, initial, step, lb, {datum::inf}, nsteps, traj);
-  
-  Rcpp::List ret = Rcpp::List::create(Rcpp::Named("final")=post.final,
-                                      Rcpp::Named("final.p")=post.finalp,
-                                      Rcpp::Named("lpr")=post.lprvalue,
-                                      Rcpp::Named("step")=post.step,
-                                      Rcpp::Named("apr")=post.apr,
-                                      Rcpp::Named("acc")=post.acc,
-                                      Rcpp::Named("delta")=post.delta);
-  if(traj){
-    ret.push_back(post.trajp, "traj.p");
-    ret.push_back(post.trajq, "traj.q");
-    ret.push_back(post.trajH, "traj.H");
-  }
   return ret;
 }
 
@@ -171,10 +195,13 @@ Rcpp::List xthetaphi1sigmaSample( const arma::mat & xInitial,
 // [[Rcpp::export]]
 Rcpp::List xthetaphi1sigmallikRcpp( const arma::mat & xlatent, 
                                     const arma::vec & theta, 
-                                    const arma::mat & phi, 
+                                    const arma::vec & phi1, 
                                     const arma::vec & sigma, 
                                     const arma::mat & yobs, 
-                                    const arma::vec & xtimes,
+                                    const Rcpp::List & covAllDimInput,
+                                    const Rcpp::NumericVector & priorTemperatureInput = 1.0,
+                                    const bool useBand = false,
+                                    const bool useMean = false,
                                     const std::string modelName = "FN"){
   
   OdeSystem model;
@@ -182,20 +209,32 @@ Rcpp::List xthetaphi1sigmallikRcpp( const arma::mat & xlatent,
     model = OdeSystem(fnmodelODE, fnmodelDx, fnmodelDtheta, zeros(3), ones(3)*datum::inf);
   }else if(modelName == "Hes1"){
     model = OdeSystem(hes1modelODE, hes1modelDx, hes1modelDtheta, zeros(7), ones(7)*datum::inf); 
+  }else if(modelName == "Hes1-log"){
+    model = OdeSystem(hes1logmodelODE, hes1logmodelDx, hes1logmodelDtheta, zeros(7), ones(7)*datum::inf); 
   }else if(modelName == "HIV"){
     model = OdeSystem(HIVmodelODE, HIVmodelDx, HIVmodelDtheta, {-datum::inf, 0,0,0,0,0, -datum::inf,-datum::inf,-datum::inf}, ones(9)*datum::inf);   
   }else{
     throw std::runtime_error("modelName must be one of 'FN', 'Hes1', 'Hes1-log', 'HIV'");
   }
   
-  lp ret = xthetaphisigmallik(xlatent, 
-                              theta, 
-                              phi, 
-                              sigma, 
-                              yobs, 
-                              xtimes,
-                              model);
+  vector<gpcov> covAllDimensions(yobs.n_cols);
+  for(unsigned j = 0; j < yobs.n_cols; j++){
+    covAllDimensions[j] = cov_r2cpp(covAllDimInput[j]);
+  }
+  
+  const arma::vec priorTemperature = Rcpp::as<arma::vec>(priorTemperatureInput);
+  
+  lp ret = xthetaphi1sigmallik(xlatent, 
+                               theta, 
+                               phi1,
+                               sigma, 
+                               yobs, 
+                               covAllDimensions,
+                               model,
+                               priorTemperature,
+                               useBand,
+                               useMean);
+  
   return Rcpp::List::create(Rcpp::Named("value")=ret.value,
                             Rcpp::Named("grad")=ret.gradient);
 }
-
