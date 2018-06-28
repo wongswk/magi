1baf90c936f1702bfec179ee91f6b160d3b91eba
shihao-yang <shihaoyang@g.harvard.edu>
2018-06-28 11:41:47 +0800

    summary and folder rename

--- R/hes1-odyssey-summary.R
+++ R/hes1-odyssey-summary.R
@@ -3,11 +3,12 @@ library(gpds)
 outDir <- "/n/regal/kou_lab/shihaoyang/DynamicSys/results/Hes1-log-withmeanBand-generalMatern-nobs33-noise0.15_0.15_0.15-ndis33/"
 outDir <- "/n/regal/kou_lab/shihaoyang/DynamicSys/results/Hes1-log-withmeanBand-generalMatern-nobs33-noise0.15_0.15_0.1-ndis33/"
 outDir <- "/n/regal/kou_lab/shihaoyang/DynamicSys/results/Hes1-withmeanBand-generalMatern-nobs33-noise0.8_0.4_1-ndis33/"
+outDir <- "/n/regal/kou_lab/shihaoyang/DynamicSys/results/Hes1-withmeanBand-generalMatern-nobs33-noise0.8_0.4_1.5-ndis33/"
 
 allf <- list.files(outDir)
 allf <- allf[grep("\\.rds", allf)]
 # allf <- allf[grep("fully-observed", allf)]
-allf <- allf[grep("partial-observed", allf)]
+# allf <- allf[grep("partial-observed", allf)]
 
 ci <- sapply(allf, function(f) readRDS(file.path(outDir, f)), simplify = "array")
 label <- dimnames(ci)[[3]]
@@ -24,6 +25,8 @@ label_numeric[is.na(label_numeric)] <- 1
 
 sizeSummary[order(label_numeric)]
 resultSummary[,,order(label_numeric)]
+outDir <- substr(outDir, 1, nchar(outDir)-1)
+system(paste0("mv ", outDir, " ", outDir, format(Sys.time(), "%Y-%m-%d_%H-%M-%S")))
 
 
 output <- round(t(apply(ci, 1:2, mean)), 4)
