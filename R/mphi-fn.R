d4f0f3b88cc2bf1f8e3756fae5f20f1a638ee9e4
shihao-yang <shihaoyang@g.harvard.edu>
2018-08-04 12:09:09 -0400

    first few chain moves to see why bias

--- R/mphi-fn.R
+++ R/mphi-fn.R
@@ -170,3 +170,33 @@ stanode$fode <- aperm(stanode$fode, c(3,1,2))
 gpds:::plotPostSamplesFlex(
   paste0("m-finisteDifference-stan.pdf"), 
   xtrue, dotxtrue, xsim, stanode, pram.true, config, odemodel = odemodel)
+
+save.image(paste0(filename, "-stan.rda"))
+
+# likelihood move away from mode ------------------------------------------------
+gpsmooth <- stan(file="stan/m-finiteDifference.stan",
+                 data=list(
+                   N=nrow(xsim),
+                   vobs=xsim$X1,
+                   robs=xsim$X2,
+                   time=xsim$time,
+                   sigma_obs=stanConfig$sigma_obs,
+                   sigma_xdot=stanConfig$sigma_xdot
+                 ),
+                 iter=200, chains=1, init=rep(list(init), 1), warmup = 20)
+
+gpsmooth_ss <- extract(gpsmooth, permuted=TRUE)
+
+stanode <- list(theta=gpsmooth_ss$abc,
+                xsampled=abind::abind(gpsmooth_ss$vtrue, gpsmooth_ss$rtrue, along = 3),
+                lglik=gpsmooth_ss$lp__,
+                sigma = pram.true$sigma)
+stanode$fode <- sapply(1:length(stanode$lglik), function(t) 
+  with(stanode, gpds:::fnmodelODE(theta[t,], xsampled[t,,])), simplify = "array")
+stanode$fode <- aperm(stanode$fode, c(3,1,2))
+
+gpds:::plotPostSamplesFlex(
+  paste0("m-finisteDifference-likelihoodmove-stan.pdf"), 
+  xtrue, dotxtrue, xsim, stanode, pram.true, config)
+
+
