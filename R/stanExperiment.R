d1f4e69cac4a89325ec5cad756b891461019a081
shihao-yang <shihaoyang@g.harvard.edu>
2018-08-12 12:59:42 -0400

    more kernel to try

--- R/stanExperiment.R
+++ R/stanExperiment.R
@@ -3,20 +3,20 @@ library(gpds)
 nobs.candidates <- c(101, 201)
 noise.candidates <- c(0.001, 0.5, 0.5, 0.5)
 filllevel.candidates <- 0:3
-maternDf.candidates <- seq(2.1, 4.5, 0.1)
+kernel.candidates <- c("generalMatern-2.01", "generalMatern-2.05", "rbf", 
+                       "finiteDifference1h", "finiteDifference2h")
 
-indicatorArray <- array(FALSE, dim=c(length(maternDf.candidates), 
+indicatorArray <- array(FALSE, dim=c(length(kernel.candidates), 
                                      length(noise.candidates), 
                                      length(nobs.candidates),
                                      length(filllevel.candidates) ))
+
 arg <- commandArgs(trailingOnly = TRUE)
 arg <- as.numeric(arg) %% length(indicatorArray) + 1
 indicatorArray[arg] <- TRUE
 
 seed <- (which(apply(indicatorArray, 2, any)) * 10 +
            which(apply(indicatorArray, 3, any)) * 100)
-  
-thisDf <- maternDf.candidates[apply(indicatorArray, 1, any)]
 
 config <- list(
   nobs = nobs.candidates[apply(indicatorArray, 3, any)],
@@ -25,7 +25,7 @@ config <- list(
   npostplot = 50,
   filllevel = filllevel.candidates[apply(indicatorArray, 4, any)],
   modelName = "FN",
-  kernel = paste0("generalMatern-", round(thisDf, 2))
+  kernel = kernel.candidates[apply(indicatorArray, 1, any)]
 )
 config$ndis <- (config$nobs-1)*2^config$filllevel+1
 
