65cad5e94287f244c27d3616a5964f594e0d3947
shihao-yang <shihaoyang@g.harvard.edu>
2018-08-20 23:12:52 -0400

    dropout experiment

--- odyssey/job_array.sh
+++ odyssey/job_array.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -J DynamicSystem # A single job name for the array
-#SBATCH -n 7 # Number of cores
+#SBATCH -n 1 # Number of cores
 #SBATCH -N 1 # All cores on one machine
 #SBATCH -p serial_requeue # Partition
-#SBATCH --mem 4000 # Memory request
-#SBATCH -t 1-01:20 # (D-HH:MM)
-#SBATCH -o /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.out # Standard output
-#SBATCH -e /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.err # Standard error
+#SBATCH --mem 5000 # Memory request
+#SBATCH -t 2-01:20 # (D-HH:MM)
+#SBATCH -o /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystemDropout%a.out # Standard output
+#SBATCH -e /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystemDropout%a.err # Standard error
 
-Rscript R/stanExperiment.R ${SLURM_ARRAY_TASK_ID}
+Rscript R/dropoutExperiment.R ${SLURM_ARRAY_TASK_ID}
