1de2810225cbdaf6ea27806c177b96b19c1d0f4d
shihaoyang <sean.shihao.yang@gmail.com>
2019-05-28 22:20:11 -0500

    add git submodules and dependencies

---
+++ tools/dependencies.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+export CPU=$(python -c 'import multiprocessing as mp; print(mp.cpu_count())')
+export MAKE="make -j $CPU"
+
+PROJECT=$(pwd)
+ARMADILLO=armadillo-code
+
+#make sure submodules are installed
+git submodule init
+git submodule sync
+git submodule update
+
+cd $PROJECT
+
+if [ ! -d "include" ]; then
+    mkdir include
+fi
+if [ ! -d "lib" ]; then
+    mkdir lib
+fi
+if [ ! -d "docs" ]; then
+    mkdir docs
+fi
+
+if [ ! -d "package" ]; then
+    mkdir package
+fi
+
+if [ ! -f "lib/libarmadillo.dylib" ] && [ ! -f "lib/libarmadillo.so" ]; then
+    cd package
+    git clone https://gitlab.com/conradsnicta/armadillo-code.git
+    cd armadillo-code
+    git checkout 8.500.x
+    ./configure
+    make -j $CPU
+    cp -Rf include/* ../../include/
+    cp libarmadillo.* ../../lib/
+    cd $PROJECT
+fi
+
+if [ ! -d "include/pybind11" ]; then
+    cd package
+    git clone https://github.com/pybind/pybind11
+    git checkout v2.2.2
+    cp -r package/pybind11/include/* include/
+fi
