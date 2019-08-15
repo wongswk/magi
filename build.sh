cc6b0d5fc130883c80a463faa66016182f21328b
shihaoyang <sean.shihao.yang@gmail.com>
2019-08-14 22:46:57 -0400

    codecov path bug fix

--- build.sh
+++ build.sh
@@ -38,6 +38,6 @@ export CODECOV_TOKEN="7b481576-694c-4591-8370-64f61df55bdc"
 cd rgpds
 ./r_build.sh
 Rscript -e 'devtools::test()'
-Rscript -e 'covr::codecov(path = "rgpds")'
+Rscript -e 'covr::codecov(path = ".")'
 
 cd $PROJECT
