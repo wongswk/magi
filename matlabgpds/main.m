1e86faa880711985c2d8414f0104a8b80dfd3ccd
shihaoyang <sean.shihao.yang@gmail.com>
2019-08-16 01:00:23 -0400

    compiler has only one error about function lambda

--- matlabgpds/main.m
+++ matlabgpds/main.m
@@ -1,2 +1,3 @@
 mex -v '-I../include' '-I../gpds_cpp' '-L../gpds_cpp' '-lcgpds' GCC='g++-6' src/maternCovTest.cpp;
+mex -v '-I../include' '-I../gpds_cpp' '-L../gpds_cpp' '-lcgpds' GCC='g++-6' src/basic_hmcC.cpp;
 maternCovTest([1,1],[1 2; 2 1])
