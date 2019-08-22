c686889b4cbc9cfde5c671fe4ebb0bde916f7436
shihaoyang <sean.shihao.yang@gmail.com>
2019-08-22 11:23:34 -0400

    proper indentation

--- gpds_cpp/classDefinition.h
+++ gpds_cpp/classDefinition.h
@@ -5,78 +5,78 @@
 #include <armadillo>
 // #include <armadillo> // uncomment this for pure c++ compilation 
 #include <iostream>
-#include <stdio.h>
+#include <cstdio>
 
 struct hmcstate{
-  arma::vec final, finalp, step, trajH;
-  double lprvalue, apr, delta;
-  int acc;
-  arma::mat trajq, trajp;
+    arma::vec final, finalp, step, trajH;
+    double lprvalue, apr, delta;
+    int acc;
+    arma::mat trajq, trajp;
 };
 
 struct lp{
-  double value;
-  arma::vec gradient;
-  lp (const double & tgtv) : value(tgtv) {}
-  lp (){}
-  lp (const lp & lp2) : value(lp2.value), gradient(lp2.gradient) {}
+    double value;
+    arma::vec gradient;
+    lp (const double & tgtv) : value(tgtv) {}
+    lp (){}
+    lp (const lp & lp2) : value(lp2.value), gradient(lp2.gradient) {}
 };
 
 struct mcmcstate {
-  arma::vec state;
-  double lpv;
-  int acc;
-  mcmcstate(const hmcstate & x){
-    state = x.final;
-    lpv = x.lprvalue;
-    acc = x.acc;
-  }
-  mcmcstate(){}
-  mcmcstate(const mcmcstate & another){
-    state = another.state;
-    lpv = another.lpv;
-    acc = another.acc;
-  }
+    arma::vec state;
+    double lpv;
+    int acc;
+    mcmcstate(const hmcstate & x){
+        state = x.final;
+        lpv = x.lprvalue;
+        acc = x.acc;
+    }
+    mcmcstate(){}
+    mcmcstate(const mcmcstate & another){
+        state = another.state;
+        lpv = another.lpv;
+        acc = another.acc;
+    }
 };
 
 struct gpcov {
-  arma::mat C, Cprime, Cdoubleprime, Cinv, mphi, Kphi, Kinv, CeigenVec, KeigenVec, mphiLeftHalf;
-  arma::mat Sigma;
-  arma::cube dCdphiCube, dCprimedphiCube, dCdoubleprimedphiCube, dSigmadphiCube;
-  arma::mat CinvBand, mphiBand, KinvBand;
-  arma::vec Ceigen1over, Keigen1over, mu, dotmu;
-  int bandsize;
+    arma::mat C, Cprime, Cdoubleprime, Cinv, mphi, Kphi, Kinv, CeigenVec, KeigenVec, mphiLeftHalf;
+    arma::mat Sigma;
+    arma::cube dCdphiCube, dCprimedphiCube, dCdoubleprimedphiCube, dSigmadphiCube;
+    arma::mat CinvBand, mphiBand, KinvBand;
+    arma::vec Ceigen1over, Keigen1over, mu, dotmu;
+    int bandsize;
 };
 
 class OdeSystem {
 public:
-  // row is observations, col is each X variable
-  std::function<arma::mat (arma::vec, arma::mat)> fOde;
-  // row is observations, col is each partial X denominator, slice is each X variable numerator
-  std::function<arma::cube (arma::vec, arma::mat)> fOdeDx;
-  // row is observations, col is each partial theta denominator, slice is each X variable numerator
-  std::function<arma::cube (arma::vec, arma::mat)> fOdeDtheta;
-  
-  std::string name;
-  
-  arma::vec thetaLowerBound;
-  arma::vec thetaUpperBound;
-  unsigned int thetaSize;
-  
-  arma::vec xLowerBound;
-  arma::vec xUpperBound;
-  
-  OdeSystem(
-    const std::function<arma::mat (arma::vec, arma::mat)> & fOdeInput,
-    const std::function<arma::cube (arma::vec, arma::mat)> & fOdeDxInput,
-    const std::function<arma::cube (arma::vec, arma::mat)> & fOdeDthetaInput,
-    const arma::vec & thetaLowerBoundInput,
-    const arma::vec & thetaUpperBoundInput
-  ) : fOde(fOdeInput), fOdeDx(fOdeDxInput), fOdeDtheta(fOdeDthetaInput), 
-  thetaLowerBound(thetaLowerBoundInput), thetaUpperBound(thetaUpperBoundInput), thetaSize(thetaLowerBoundInput.size()) {};
-  
-  OdeSystem() {};
-  bool checkBound(const arma::mat & xlatent, const arma::vec & theta, lp* retPtr) const;
+    // row is observations, col is each X variable
+    std::function<arma::mat (arma::vec, arma::mat)> fOde;
+    // row is observations, col is each partial X denominator, slice is each X variable numerator
+    std::function<arma::cube (arma::vec, arma::mat)> fOdeDx;
+    // row is observations, col is each partial theta denominator, slice is each X variable numerator
+    std::function<arma::cube (arma::vec, arma::mat)> fOdeDtheta;
+
+    std::string name;
+
+    arma::vec thetaLowerBound;
+    arma::vec thetaUpperBound;
+    unsigned int thetaSize;
+
+    arma::vec xLowerBound;
+    arma::vec xUpperBound;
+
+    OdeSystem(
+            const std::function<arma::mat (arma::vec, arma::mat)> & fOdeInput,
+            const std::function<arma::cube (arma::vec, arma::mat)> & fOdeDxInput,
+            const std::function<arma::cube (arma::vec, arma::mat)> & fOdeDthetaInput,
+            const arma::vec & thetaLowerBoundInput,
+            const arma::vec & thetaUpperBoundInput
+    ) : fOde(fOdeInput), fOdeDx(fOdeDxInput), fOdeDtheta(fOdeDthetaInput),
+        thetaLowerBound(thetaLowerBoundInput), thetaUpperBound(thetaUpperBoundInput), thetaSize(thetaLowerBoundInput.size()) {};
+
+    OdeSystem() {};
+    bool checkBound(const arma::mat & xlatent, const arma::vec & theta, lp* retPtr) const;
 };
 
 #endif
