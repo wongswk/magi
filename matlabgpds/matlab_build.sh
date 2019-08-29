af865d277183c60aa097994da2318a0efec82068
Samuel <samuel.wong@uwaterloo.ca>
2019-08-28 21:58:41 -0400

    matlab cleanup and add ptrans example

--- matlabgpds/matlab_build.sh
+++ matlabgpds/matlab_build.sh
@@ -12,4 +12,4 @@ fi
 cd "$PROJECT"/matlabgpds || exit 1
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PROJECT/gpds_cpp
-matlab -nodisplay -nosplash -nodesktop -r "mex -v '-I../include' '-I../gpds_cpp' '-L../gpds_cpp' '-lcgpds' GCC='g++-6' src/maternCovTest.cpp;exit;"
+matlab -nodisplay -nosplash -nodesktop -r "mex -v '-I../include' '-I../gpds_cpp' '-L../gpds_cpp' '-lcgpds' GCC='g++-6' src/solveGpds.cpp;exit;"
