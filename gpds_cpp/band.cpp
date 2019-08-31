cb05835746cc74fa283775e1f1ef48122a1bbc64
shihaoyang <sean.shihao.yang@gmail.com>
2019-08-31 11:13:18 -0400

    new band that works with matlab

--- gpds_cpp/band.cpp
+++ gpds_cpp/band.cpp
@@ -5,36 +5,39 @@ typedef int blasint;
 extern "C" {
   
   void dgbmv_(const char *, const blasint *, const blasint *, const blasint *, const blasint *, const double *, const double *, const blasint *,
-                const double *, const blasint *, const double *, double *, const blasint *);
-
+              const double *, const blasint *, const double *, double *, const blasint *);
+  
   void bmatvecmult(const double *a, const double *b, const int *bandsize, const int *matdim, double *result) {
-
+    
     double zero = 0.0;
     double one = 1.0;
-    int ione = 1;
+    blasint ione = 1;
     char nthechar = 'n';
     //cout << *a << " " << *bandsize << " " << *matdim <<  endl;
-    int nrow = 2 * *bandsize +  1;
-
+    blasint nrow = 2 * *bandsize +  1;
+    blasint bmatdim = *matdim;
+    blasint bbandsize = *bandsize;
     //cout << nrow << endl;
-
-    dgbmv_(&nthechar, matdim, matdim, bandsize, bandsize, &one, a, &nrow, b, &ione, &zero, result,
-             &ione);
-
-
+    
+    dgbmv_(&nthechar, &bmatdim, &bmatdim, &bbandsize, &bbandsize, &one, a, &nrow, b, &ione, &zero, result,
+           &ione);
+    
+    
   }
-
+  
   void bmatvecmultT(const double *a, const double *b, const int *bandsize, const int *matdim, double *result) {
-
+    
     double zero = 0.0;
     double one = 1.0;
-    int ione = 1;
-    int nrow = 2 * *bandsize +  1;
+    blasint ione = 1;
+    blasint nrow = 2 * *bandsize +  1;
     char tthechar = 't';
-
-    dgbmv_(&tthechar, matdim, matdim, bandsize, bandsize, &one, a, &nrow, b, &ione, &zero, result,
-             &ione);
-
+    blasint bmatdim = *matdim;
+    blasint bbandsize = *bandsize;    
+    
+    dgbmv_(&tthechar,  &bmatdim, &bmatdim, &bbandsize, &bbandsize, &one, a, &nrow, b, &ione, &zero, result,
+           &ione);
+    
   }
 
 
