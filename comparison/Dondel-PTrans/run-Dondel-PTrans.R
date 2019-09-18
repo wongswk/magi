ac062c6b5e751c6fbb84f1effc31fb99911a7b8d
Samuel <samuel.wong@uwaterloo.ca>
2019-09-17 23:30:28 -0400

    update Dondel PTrans to use our seed and setup

--- comparison/Dondel-PTrans/run-Dondel-PTrans.R
+++ comparison/Dondel-PTrans/run-Dondel-PTrans.R
@@ -1,3 +1,69 @@
+#### run with priorTempered phase 1 --------------------------------------------
+library(gpds)
+# set up configuration if not already exist ------------------------------------
+if(!exists("config")){
+  config <- list(
+    nobs = 15,
+    noise = rep(0.001, 5), # 0.001 = low noise, 0.01 = high noise
+    kernel = "generalMatern",
+    seed = 1365546660, #(as.integer(Sys.time())*104729+sample(1e9,1))%%1e9,
+    #loglikflag = "withmeanBand",
+    #bandsize = 40,
+    #hmcSteps = 100,
+    n.iter = 300001,
+    burninRatio = 0.50,
+    #stepSizeFactor = 0.06,
+    #filllevel = 3,
+    #linfillspace = 0.1,
+    t.end = 100,
+    modelName = "PTrans-Dondel"
+    #temperPrior = TRUE,
+    #useFrequencyBasedPrior = TRUE,
+    #useScalerSigma = FALSE,
+    #useFixedSigma = FALSE,
+    #linearizexInit = TRUE,
+    #useExoSigma = TRUE,
+    #max.epoch = 1
+  )
+}
+
+# initialize global parameters, true x, simulated x ----------------------------
+pram.true <- list(
+  theta=c(0.07, 0.6,0.05,0.3,0.017,0.3),
+  x0 = c(1,0,1,0,0),
+  sigma=config$noise
+)
+
+times <- seq(0,100,length=1001)
+
+modelODE <- function(t, state, parameters) {
+  list(as.vector(gpds:::ptransmodelODE(parameters, t(state))))
+}
+
+xtrue <- deSolve::ode(y = pram.true$x0, times = times, func = modelODE, parms = pram.true$theta)
+xtrue <- data.frame(xtrue)
+matplot(xtrue[, "time"], xtrue[, -1], type="l", lty=1)
+
+xtrueFunc <- lapply(2:ncol(xtrue), function(j)
+  approxfun(xtrue[, "time"], xtrue[, j]))
+
+xsim <- data.frame(time = c(0,1,2,4,5,7,10,15,20,30,40,50,60,80,100))
+xsim <- cbind(xsim, sapply(xtrueFunc, function(f) f(xsim$time)))
+
+set.seed(config$seed)
+for(j in 1:(ncol(xsim)-1)){
+  xsim[,1+j] <- xsim[,1+j]+rnorm(nrow(xsim), sd=config$noise[j])  
+}
+
+ptransmodel <- list(
+  fOde=gpds:::ptransmodelODE,
+  fOdeDx=gpds:::ptransmodelDx,
+  fOdeDtheta=gpds:::ptransmodelDtheta,
+  thetaLowerBound=rep(0,6),
+  thetaUpperBound=rep(100,6)
+)
+
+
 ## Dondelinger demo on PTrans.
 
 library(deSolve)
@@ -15,64 +81,18 @@ VG_func <- function(t, X, params) {
 }
 
 # Generate data
-timeTest =c(0,1,2,4,5,7,10,15,20,30,40,50,60,80,100)
+timeTest <- xsim[,1]  #c(0,1,2,4,5,7,10,15,20,30,40,50,60,80,100)
+dataTest <- as.matrix(xsim[,2:ncol(xsim)])
 
 ## noise 0.01 case
-dataTest = ode(c(1,0,1,0,0), timeTest,function(t,y,params)list(VG_func(t,matrix(y,1,length(y)),params)),c(0.07,0.6,0.05,0.3,0.017,0.3))
-dataTest = dataTest[,2:6] +rnorm(dim(dataTest)[1]*5,0,0.01)
-agm.result =agm(data=dataTest,time=timeTest,ode.system=VG_func, numberOfParameters=6, noise.sd = 0.01, maxIterations = 100000, showProgress = TRUE)
+#dataTest = ode(c(1,0,1,0,0), timeTest,function(t,y,params)list(VG_func(t,matrix(y,1,length(y)),params)),c(0.07,0.6,0.05,0.3,0.017,0.3))
+#dataTest = dataTest[,2:6] +rnorm(dim(dataTest)[1]*5,0,0.01)
+agm.result =agm(data=dataTest,time=timeTest,ode.system=VG_func, numberOfParameters=length(pram.true$theta), noise.sd = config$noise[1], maxIterations = config$n.iter, showProgress = TRUE)
 ### you have to input noise.sd, method does not sample it
 
 
 #### Use our plotting codes
-library(gpds)
-
-  config <- list(
-    nobs = 15,
-    noise = rep(0.01, 5), # 0.001 = low noise, 0.01 = high noise
-    seed = 12345,   #### not used just placeholder
-    n.iter = 4000,   ## since 100000 divided by 25 chains
-    burninRatio = 0.5,
-    t.end = 100,
-    modelName = "PTrans-Dondel"
-  )
-
-# initialize global parameters, true x, simulated x ----------------------------
-pram.true <- list(
-  theta=c(0.07, 0.6,0.05,0.3,0.017,0.3),
-  x0 = c(1,0,1,0,0),
-  sigma=config$noise
-)
-
-times <- seq(0,config$t.end,length=1001)
-
-modelODE <- function(t, state, parameters) {
-  list(as.vector(gpds:::ptransmodelODE(parameters, t(state))))
-}
-
-xtrue <- deSolve::ode(y = pram.true$x0, times = times, func = modelODE, parms = pram.true$theta)
-xtrue <- data.frame(xtrue)
-matplot(xtrue[, "time"], xtrue[, -1], type="l", lty=1)
-
-xtrueFunc <- lapply(2:ncol(xtrue), function(j)
-  approxfun(xtrue[, "time"], xtrue[, j]))
-
-xsim <- data.frame(time = c(0,1,2,4,5,7,10,15,20,30,40,50,60,80,100))
-xsim <- cbind(xsim, sapply(xtrueFunc, function(f) f(xsim$time)))
-
-xsim.obs <- cbind(xsim[,"time"], dataTest)   #### read Dondel observations
-colnames(xsim.obs) <- c("time", 1:(ncol(xsim)-1))
-matplot(xsim.obs[,1], xsim.obs[,-1], type="p", col=1:(ncol(xsim)-1), pch=20, add = TRUE)
-
-matplot(xsim.obs[,1], xsim.obs[,-1], type="p", col=1:(ncol(xsim)-1), pch=20)
-
-ptransmodel <- list(
-  fOde=gpds:::ptransmodelODE,
-  fOdeDx=gpds:::ptransmodelDx,
-  fOdeDtheta=gpds:::ptransmodelDtheta,
-  thetaLowerBound=rep(0,6),
-  thetaUpperBound=rep(100,6)
-)
+config$n.iter <- config$n.iter / 25
 
 xsampled <- (sapply(agm.result$x.samples, function(x) x, simplify="array"))
 thetasampled <- agm.result$posterior.samples
