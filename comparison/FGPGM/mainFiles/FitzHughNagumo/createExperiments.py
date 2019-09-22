a2047ae04ef2807c88feb4424511aca2a8d131d8
shihaoyang <sean.shihao.yang@gmail.com>
2019-09-21 21:36:36 -0400

    odyssey job array running

--- comparison/FGPGM/mainFiles/FitzHughNagumo/createExperiments.py
+++ comparison/FGPGM/mainFiles/FitzHughNagumo/createExperiments.py
@@ -5,11 +5,19 @@ Creates and saves all the files used later by FGPGM.
 Must be run from the testRun{}.format{i} directories.
 """
 
+import argparse
 import numpy as np
 from FGPGM.Experiments.FitzHughNagumo import FHN
 
 from matplotlib import pyplot as plt
 plt.switch_backend('agg')
+
+
+"""parse the command line argument"""
+parser = argparse.ArgumentParser()
+parser.add_argument('--slurm_array_task_id', help='slurm_array_task_id of job array on odyssey', default=-1, type=int)
+args = parser.parse_args()
+
 
 """seed random number generator and save the seed for reproducability"""
 seed = np.random.randint(0, 2**32-1)
@@ -28,7 +36,12 @@ np.savetxt("trueODEParams.csv", theta)
 
 print("normal time")
 tEnd = 10 # 5
-dt = 0.25 # 0.125
+dt_candidates = [0.5, 0.4, 0.25, 0.2, 0.1]
+if args.slurm_array_task_id < 0:
+    dt = 0.25 # 0.125
+else:
+    dt = dt_candidates[args.slurm_array_task_id % len(dt_candidates)]
+    args.slurm_array_task_id = args.slurm_array_task_id // len(dt_candidates)
 time = np.arange(0, tEnd+0.5*dt, dt)  # obs time
 time = np.asarray(time)
 np.savetxt("time.csv", time)
@@ -39,7 +52,12 @@ np.savetxt("standardize.csv", np.asarray(standardize).reshape([1, 1]))
 # obsStd = 0.2
 # np.savetxt("obsStdInput.csv", np.asarray(obsStd).reshape([1, 1]))
 
-SNR = 10
+# SNR = np.loadtxt("SNRInput.csv")
+snr_candidates = [10, 100]
+if args.slurm_array_task_id < 0:
+    SNR = 10
+else:
+    SNR = snr_candidates[args.slurm_array_task_id % len(snr_candidates)]
 np.savetxt("usedSNR.csv", np.asarray(SNR).reshape([1, 1]))
 
 experiment = FHN()
---
+++ comparison/Wenk-FN/odyssey.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J DynamicSystem # A single job name for the array
+#SBATCH -n 26 # Number of cores
+#SBATCH -N 1 # All cores on one machine
+#SBATCH -p serial_requeue # Partition
+#SBATCH --mem 8000 # Memory request
+#SBATCH -t 1-01:20 # (D-HH:MM)
+#SBATCH -o /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.out # Standard output
+#SBATCH -e /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.err # Standard error
+
+
+export PYTHONPATH="$HOME/Workspace/DynamicSys/dynamic-systems/comparison/"
+source "$HOME/Workspace/DynamicSys/pyenv_gpds/bin/activate"
+mkdir -p "$PYTHONPATH/Wenk-FN/Wenk19-FN-${SLURM_ARRAY_TASK_ID}" && cd "$PYTHONPATH/Wenk-FN/Wenk19-FN-${SLURM_ARRAY_TASK_ID}"
+
+python3 $PYTHONPATH/FGPGM/mainFiles/FitzHughNagumo/createExperiments.py --slurm_array_task_id $SLURM_ARRAY_TASK_ID
+python3 $PYTHONPATH/FGPGM/mainFiles/FitzHughNagumo/getHyperparams.py
+python3 $PYTHONPATH/FGPGM/mainFiles/FitzHughNagumo/doFGPGM.py
