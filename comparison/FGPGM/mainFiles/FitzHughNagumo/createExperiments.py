6116cc7aeee9e36e1a767aefade713ddd390321a
shihaoyang <sean.shihao.yang@gmail.com>
2019-09-27 00:55:14 -0400

    add command line argument

--- comparison/FGPGM/mainFiles/FitzHughNagumo/createExperiments.py
+++ comparison/FGPGM/mainFiles/FitzHughNagumo/createExperiments.py
@@ -16,6 +16,10 @@ plt.switch_backend('agg')
 """parse the command line argument"""
 parser = argparse.ArgumentParser()
 parser.add_argument('--slurm_array_task_id', help='slurm_array_task_id of job array on odyssey', default=-1, type=int)
+parser.add_argument('--tEnd', help='tEnd of simulation', default=10, type=float)
+parser.add_argument('--dt', help='time increment', default=0.25, type=float)
+parser.add_argument('--obsNoiseStd', help='obsNoiseStd of simulation', default=None, type=float)
+parser.add_argument('--SNR', help='SNR of simulation', default=10, type=float)
 args = parser.parse_args()
 
 
@@ -35,10 +39,10 @@ theta = np.asarray(theta)
 np.savetxt("trueODEParams.csv", theta)
 
 print("normal time")
-tEnd = 10 # 5
+tEnd = args.tEnd # 5
 dt_candidates = [0.5, 0.4, 0.25, 0.2, 0.1]
 if args.slurm_array_task_id < 0:
-    dt = 0.25 # 0.125
+    dt = args.dt
 else:
     dt = dt_candidates[args.slurm_array_task_id % len(dt_candidates)]
     args.slurm_array_task_id = args.slurm_array_task_id // len(dt_candidates)
@@ -55,14 +59,18 @@ np.savetxt("standardize.csv", np.asarray(standardize).reshape([1, 1]))
 # SNR = np.loadtxt("SNRInput.csv")
 snr_candidates = [10, 100]
 if args.slurm_array_task_id < 0:
-    SNR = 10
+    SNR = args.SNR
+    obsNoiseStd = args.obsNoiseStd
 else:
     SNR = snr_candidates[args.slurm_array_task_id % len(snr_candidates)]
-np.savetxt("usedSNR.csv", np.asarray(SNR).reshape([1, 1]))
+    obsNoiseStd = None
+with open("config.txt","w") as file:
+    file.write(str(args))
+
 
 experiment = FHN()
 # sample noisy and true states at observation times
-x, y = experiment.sampleTrajectoryNonUniform(XInit, theta, time, obsNoiseStd=None,
+x, y = experiment.sampleTrajectoryNonUniform(XInit, theta, time, obsNoiseStd=obsNoiseStd,
                                              SNR=SNR, plotting=None)
 
 np.savetxt("trueStates.csv", x)
