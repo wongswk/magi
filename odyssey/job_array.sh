697f778c1820f9a2be6270927751a0681da8107c
shihaoyang <sean.shihao.yang@gmail.com>
2019-11-14 18:51:23 -0500

    new job array to add ramsay samples

--- odyssey/job_array.sh
+++ odyssey/job_array.sh
@@ -3,10 +3,10 @@
 #SBATCH -n 1 # Number of cores
 #SBATCH -N 1 # All cores on one machine
 #SBATCH -p serial_requeue # Partition
-#SBATCH --mem 4000 # Memory request
+#SBATCH --mem 8000 # Memory request
 #SBATCH -t 1-01:20 # (D-HH:MM)
-#SBATCH -o /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.out # Standard output
-#SBATCH -e /n/regal/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.err # Standard error
+#SBATCH -o /n/scratchlfs/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.out # Standard output
+#SBATCH -e /n/scratchlfs/kou_lab/shihaoyang/dynamic_sys/DynamicSystem%a.err # Standard error
 
 # if (( $SLURM_ARRAY_TASK_ID % 4 == 0 ))
 # then
@@ -28,5 +28,6 @@
 #   Rscript R/hes1-async-partial-observations.R ${SLURM_ARRAY_TASK_ID}
 # fi
 
-Rscript R/largeParameterExperiment.R ${SLURM_ARRAY_TASK_ID}
+# Rscript R/largeParameterExperiment.R ${SLURM_ARRAY_TASK_ID}
 # Rscript R/fn-ramsay.R ${SLURM_ARRAY_TASK_ID}
+Rscript R/addRamsayFN.R ${SLURM_ARRAY_TASK_ID}
