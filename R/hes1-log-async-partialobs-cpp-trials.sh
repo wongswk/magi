25f1af3880ffdda6b37e8b35db5164b6488203ec
shihaoyang <sean.shihao.yang@gmail.com>
2019-12-07 13:43:27 -0500

    shell script for ramsay hes1

--- R/hes1-log-async-partialobs-cpp-trials.sh
+++ R/hes1-log-async-partialobs-cpp-trials.sh
@@ -1,3 +1,13 @@
 while true; do 
   Rscript R/hes1-log-async-partialobs-cpp-trials.R &> hes1-log-async-partialobs-cpp-trials-$(date +"%m-%d-%Y-%T").log
 done
+
+while true; do 
+  Rscript R/hes1-log-fullobs-cpp.R
+done
+
+for i in {193..256}
+do
+  echo "Number: $i"
+  Rscript R/ramsey-rerun-hes1-partialobs.R $i
+done
