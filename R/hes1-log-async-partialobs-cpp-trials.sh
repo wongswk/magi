f4904c74b4fb5ecd1c0cd75e24a4571f09d90edd
Shihao Yang <shihaoyang@MBP2015>
2020-05-06 16:25:51 -0400

    script to run experiment

--- R/hes1-log-async-partialobs-cpp-trials.sh
+++ R/hes1-log-async-partialobs-cpp-trials.sh
@@ -4,6 +4,17 @@ done
 
 while true; do 
   Rscript R/hes1-log-fullobs-cpp.R
+
+sleep 2400
+for i in {1..60}
+do
+  Rscript R/hes1-log-async-partialobs-cpp-temper.R &
+done
+
+sleep 10800
+for i in {1..60}
+do
+  Rscript R/hes1-log-async-partialobs-cpp-trials.R &
 done
 
 for i in {193..256}
