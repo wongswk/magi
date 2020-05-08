75bf442e8eca9204fcbcda5fbfa7649cbb14f50e
Shihao Yang <shihaoyang@MBP2015>
2020-05-07 23:58:17 -0400

    phi investigation show some promising results for re-optimize phi after warm start

--- R/hes1-log-phi-investigation.R
+++ R/hes1-log-phi-investigation.R
@@ -1,84 +1,12 @@
 library(gpds)
 # set up configuration if not already exist ------------------------------------
-if(!exists("config")){
-  config <- list(
-    nobs = 33,
-    noise = c(0.15,0.15,0.1),
-    kernel = "generalMatern",
-    seed = (as.integer(Sys.time())*104729+sample(1e9,1))%%1e9,
-    npostplot = 50,
-    loglikflag = "withmeanBand",
-    bandsize = 20,
-    hmcSteps = 500,
-    n.iter = 2e2,
-    burninRatio = 0.50,
-    stepSizeFactor = 0.01,
-    filllevel = 0,
-    modelName = "Hes1-log",
-    async = TRUE,
-    max.epoch = 1,
-    useMean = TRUE,
-    useBand = TRUE,
-    useFrequencyBasedPrior = TRUE,
-    useScalerSigma = FALSE,
-    useFixedSigma = TRUE
-  )
-}
 
-config$ndis <- (config$nobs-1)*2^config$filllevel+1
-config$priorTemperature <- config$ndis / config$nobs
-
-# initialize global parameters, true x, simulated x ----------------------------
-outDir <- "../results/cpp/"
-system(paste("mkdir -p", outDir))
-
-pram.true <- list(
-  theta = c(0.022, 0.3, 0.031, 0.028, 0.5, 20, 0.3),
-  x0 = log(c(1.438575, 2.037488, 17.90385)),
-  phi = c(122.4027613, 41.8511396,  
-          56.5612956, 91.4189948,
-          164.3556832, 11.9474091),
-  sigma = config$noise
-)
-times <- seq(0, 60*4, by = 0.01)
-
-modelODE <- function(t, state, parameters) {
-  list(as.vector(gpds:::hes1logmodelODE(parameters, t(state))))
-}
-
-xtrue <- deSolve::ode(y = pram.true$x0, times = times, func = modelODE, parms = pram.true$theta)
-xtrue <- data.frame(xtrue)
-matplot(xtrue[, "time"], xtrue[, -1], type="l", lty=1)
-
-xtrueFunc <- lapply(2:ncol(xtrue), function(j)
-  approxfun(xtrue[, "time"], xtrue[, j]))
-
-xsim <- xtrue
-
-set.seed(config$seed)
-for(j in 1:(ncol(xsim)-1)){
-  xsim[,1+j] <- xsim[,1+j]+rnorm(nrow(xsim), sd=config$noise[j])  
-}
-xsim$X3 <- NaN
-xsim.obs <- xsim[seq(1,nrow(xsim), length=config$nobs),]
-if(config$async){
-  xsim.obs$X1[seq(2,nrow(xsim.obs),by=2)] <- NaN
-  xsim.obs$X2[seq(1,nrow(xsim.obs),by=2)] <- NaN
-}
-matplot(xsim.obs$time, xsim.obs[,-1], type="p", col=1:(ncol(xsim)-1), pch=20, add = TRUE)
-
-xsim <- insertNaN(xsim.obs,config$filllevel)
-
-hes1logmodel <- list(
-  name="Hes1-log",
-  fOde=gpds:::hes1logmodelODE,
-  fOdeDx=gpds:::hes1logmodelDx,
-  fOdeDtheta=gpds:::hes1logmodelDtheta,
-  thetaLowerBound=rep(0,7),
-  thetaUpperBound=rep(Inf,7)
+config <- list(
+  seed = 115846061,
+  modelName = "Hes1-log"
 )
 
-## tempered with warm start and update phi from warm start ----------------------------
+## try phi smoothing ----------------------------
 
 load(paste0("../results/cpp/7param/variablephi-notemper/", config$modelName,"-",config$seed,"-7param-variablephi-notemper.rda"))
 
@@ -190,12 +118,13 @@ r.nobs <- as.matrix(dist(xsim$time))
 fn(phiUsed[,3])
 fn(phiNewInit$phi[,3])
 
+# try phi optimization ----------------------------------------------------------------------------------------
 samplesCpp <- gpds:::solveGpdsRcpp(
   yFull = data.matrix(xsim[,-1]),
   odeModel = hes1logmodel,
   tvecFull = xsim$time,
   sigmaExogenous = pram.true$sigma,
-  phiExogenous = phiNewInit$phi,
+  phiExogenous = matrix(numeric(0)),
   xInitExogenous = xInit,
   thetaInitExogenous = thetaInit,
   muExogenous = matrix(numeric(0)),
@@ -206,7 +135,7 @@ samplesCpp <- gpds:::solveGpdsRcpp(
   kernel = config$kernel,
   nstepsHmc = config$hmcSteps,
   burninRatioHmc = config$burninRatio,
-  niterHmc = config$n.iter,
+  niterHmc = 1,
   stepSizeFactorHmc = config$stepSizeFactor,
   nEpoch = config$max.epoch,
   bandSize = config$bandsize,
@@ -217,46 +146,14 @@ samplesCpp <- gpds:::solveGpdsRcpp(
   useFixedSigma = config$useFixedSigma,
   verbose = TRUE)
 
-phiUsed <- samplesCpp$phi
-samplesCpp <- samplesCpp$llikxthetasigmaSamples
-
-samplesCpp <- samplesCpp[,,1]
-out <- samplesCpp[-1,1,drop=FALSE]
-xCpp <- matrix(out[1:length(data.matrix(xsim[,-1])), 1], ncol=ncol(xsim[,-1]))
-thetaCpp <- out[(length(xCpp)+1):(length(xCpp) + length(hes1logmodel$thetaLowerBound)), 1]
-sigmaCpp <- tail(out[, 1], ncol(xsim[,-1]))
-
-matplot(xsim$time, xCpp, type="l", add=TRUE, lty=2)
-
-llikId <- 1
-xId <- (max(llikId)+1):(max(llikId)+length(data.matrix(xsim[,-1])))
-thetaId <- (max(xId)+1):(max(xId)+length(hes1logmodel$thetaLowerBound))
-sigmaId <- (max(thetaId)+1):(max(thetaId)+ncol(xsim[,-1]))
-
-
-burnin <- as.integer(config$n.iter*config$burninRatio)
-gpode <- list(theta=t(samplesCpp[thetaId, -(1:burnin)]),
-              xsampled=array(t(samplesCpp[xId, -(1:burnin)]),
-                             dim=c(config$n.iter-burnin, nrow(xsim), ncol(xsim)-1)),
-              lglik=samplesCpp[llikId,-(1:burnin)],
-              sigma = t(samplesCpp[sigmaId, -(1:burnin), drop=FALSE]))
-gpode$fode <- sapply(1:length(gpode$lglik), function(t) 
-  with(gpode, gpds:::hes1logmodelODE(theta[t,], xsampled[t,,])), simplify = "array")
-gpode$fode <- aperm(gpode$fode, c(3,1,2))
-
-dotxtrue = gpds:::hes1logmodelODE(pram.true$theta, data.matrix(xtrue[,-1]))
-
-odemodel <- list(times=times, modelODE=modelODE, xtrue=xtrue)
-
-outDir <- "../results/cpp/7param/variablephi-temper-warmstart-updatephi/"
-system(paste("mkdir -p", outDir))
-
-for(j in 1:(ncol(xsim)-1)){
-  config[[paste0("phiD", j)]] <- paste(round(phiUsed[,j], 2), collapse = "; ")
-}
-
-gpds:::plotPostSamplesFlex(
-  paste0(outDir, config$modelName,"-",config$seed,"-7param-variablephi-temper-warmstart-updatephi.pdf"), 
-  xtrue, dotxtrue, xsim, gpode, pram.true, config, odemodel)
-
-save.image(paste0(outDir, config$modelName,"-",config$seed,"-7param-variablephi-temper-warmstart-updatephi.rda"))
+# segfault below
+# phi_optimized <- gpds:::optimizePhi(
+#   yobsInput = data.matrix(xsim[,-1]), 
+#   tvecInput = xsim$time, 
+#   fOdeModelInput = hes1logmodel, 
+#   sigmaAllDimensionsInput = pram.true$sigma, 
+#   priorTemperatureInput = config$priorTemperature, 
+#   xInitInput = xInit, 
+#   thetaInitInput = thetaInit, 
+#   phiInitInput = phiUsed, 
+#   missingComponentDim = 2)
