abd7b9a04f059249a5ffa42857ee5fd4fbe618e3
Shihao Yang <shihaoyang@MBP2015>
2020-06-05 21:21:46 -0400

    fn system and hes1 both works in python

--- pygpds/test/test_solve_gpds.py
+++ pygpds/test/test_solve_gpds.py
@@ -6,7 +6,7 @@ from arma import vector, matrix
 
 
 class SolveGpdsTest(unittest.TestCase):
-    def test_solve_gpds_py(self):
+    def test_solve_fn(self):
         fn_system = OdeSystem()
         def fOde(theta, x):
             theta = vector(theta)
@@ -103,3 +103,116 @@ class SolveGpdsTest(unittest.TestCase):
             verbose = True)
         phiUsed = matrix(result_solver.phiAllDimensions)
         samplesCpp = matrix(result_solver.llikxthetasigmaSamples.slice(0))
+
+
+    def test_solve_hes1log(self):
+        hes1_system = OdeSystem()
+        def fOde(theta, x):
+            theta = vector(theta)
+            x = matrix(x)
+
+            P = np.exp(x[:, 0])
+            M = np.exp(x[:, 1])
+            H = np.exp(x[:, 2])
+
+            PMHdt = np.zeros(shape=np.shape(x))
+            PMHdt[:,0] = -theta[0]*H + theta[1]*M/P - theta[2]
+            PMHdt[:,1] = -theta[3] + theta[4]/(1+np.square(P))/M
+            PMHdt[:,2] = -theta[0]*P + theta[5]/(1+np.square(P))/H - theta[6]
+
+            return ArmaMatrix(PMHdt.T.copy())
+
+        def fOdeDx(theta, x):
+            theta = vector(theta)
+            x = matrix(x)
+
+            resultDx = np.zeros(shape=[np.shape(x)[0], np.shape(x)[1], np.shape(x)[1]])
+            P = (x[:, 0])
+            M = (x[:, 1])
+            H = (x[:, 2])
+
+            expMminusP = np.exp(M-P)
+            dP = -np.power(1+np.exp(2*P), -2) * np.exp(2*P)*2
+
+            resultDx[:,0,0] = -theta[1]*expMminusP
+            resultDx[:,1,0] = theta[1]*expMminusP
+            resultDx[:,2,0] = -theta[0]*np.exp(H)
+            
+            resultDx[:,0,1] = theta[4]*np.multiply(np.exp(-M),dP)
+            resultDx[:,1,1] = -theta[4]*np.exp(-M)/(1+np.exp(2*P))
+            
+            resultDx[:,0,2] = -theta[0]*np.exp(P) + theta[5]*np.multiply(np.exp(-H),dP)
+            resultDx[:,2,2] = -theta[5]*np.exp(-H)/(1+np.exp(2*P))
+
+            return ArmaCube(resultDx.T.copy())
+
+        def fOdeDtheta(theta, x):
+            theta = vector(theta)
+            x = matrix(x)
+
+            resultDtheta = np.zeros(shape=[np.shape(x)[0], np.shape(theta)[0], np.shape(x)[1]])
+            P = (x[:, 0])
+            M = (x[:, 1])
+            H = (x[:, 2])
+
+            resultDtheta[:,0,0] = -np.exp(H)
+            resultDtheta[:,1,0] = np.exp(M-P)
+            resultDtheta[:,2,0].fill(-1)
+
+            resultDtheta[:,3,1].fill(-1)
+            resultDtheta[:,4,1] = np.exp(-M)/(1+np.exp(2*P))
+
+            resultDtheta[:,0,2] = -np.exp(P)
+            resultDtheta[:,5,2] = np.exp(-H)/(1+np.exp(2*P))
+            resultDtheta[:,6,2].fill(-1)
+
+            return ArmaCube(resultDtheta.T.copy())
+
+        hes1_system.fOde = fOde
+        hes1_system.fOdeDx = fOdeDx
+        hes1_system.fOdeDtheta = fOdeDtheta
+        hes1_system.thetaLowerBound = ArmaVector(np.array([0,0,0,0,0,0,0]))
+        hes1_system.thetaUpperBound = ArmaVector(np.array([np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,np.inf]))
+        hes1_system.name = "Hes1-log"
+        hes1_system.thetaSize = 7
+
+
+        ydataP = [0.74, 0.78, 1.86, 1.86, 2.2, 1.93, 1.47, 1.03, 0.36,
+                  0.88, 1.68, 1.97, 2.15, 1.85, 1.8, 1.47, 0.71]
+        ydataM = [0.91, 0.82, 0.71, -0.11, 0.08, -0.45, -0.05, 0.2,
+                  0.88, 1.09, 0.3, 0.35, 0.25, -0.23, -0.51, -0.09]
+        yFull = np.ndarray([33, 3])
+        yFull.fill(np.nan)
+        yFull[np.linspace(0, 32, num=17).astype(int),0] = ydataP
+        yFull[np.linspace(1, 31, num=16).astype(int),1] = ydataM
+
+        tvecFull = np.linspace(0, 240, num=33)
+        result_solver = solveGpdsPy(
+            yFull=ArmaMatrix(yFull).t(),
+            odeModel=hes1_system,
+            tvecFull=ArmaVector(tvecFull),
+            sigmaExogenous=ArmaVector(np.array([0.15,0.15,0.15])),
+            phiExogenous = ArmaMatrix(np.ndarray([0, 0])),
+            xInitExogenous = ArmaMatrix(np.ndarray([0, 0])),
+            thetaInitExogenous = ArmaVector(np.ndarray(0)),
+            muExogenous = ArmaMatrix(np.ndarray([0, 0])),
+            dotmuExogenous = ArmaMatrix(np.ndarray([0, 0])),
+            priorTemperatureLevel = 1.0,
+            priorTemperatureDeriv = 1.0,
+            priorTemperatureObs = 1.0,
+            kernel = "generalMatern",
+            nstepsHmc = 100,
+            burninRatioHmc = 0.5,
+            niterHmc = 1000,
+            stepSizeFactorHmc = 0.1,
+            nEpoch = 1,
+            bandSize = 20,
+            useFrequencyBasedPrior = True,
+            useBand = True,
+            useMean = False,
+            useScalerSigma = False,
+            useFixedSigma = True,
+            verbose = True)
+        phiUsed = matrix(result_solver.phiAllDimensions)
+        samplesCpp = matrix(result_solver.llikxthetasigmaSamples.slice(0))
+
