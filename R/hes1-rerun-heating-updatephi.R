7cadca1de79bb7faed072775c6d6300227be3092
Shihao Yang <shihaoyang@MBP2015>
2020-07-30 16:05:10 -0400

    hes1 rerun heating updatephi

---
+++ R/hes1-rerun-heating-updatephi.R
@@ -0,0 +1,137 @@
+library(argo)
+library(deSolve)
+
+#### all possible data files
+
+subdirs <- c("../results/for_paper/7param/variablephi-heating/")
+all_files <- lapply(subdirs, list.files)
+all_files <- lapply(all_files, function(x) x[grep(".*log-([0-9]+)-7param.*", x)])
+all_seeds <- lapply(all_files, function(x) gsub(".*log-([0-9]+)-7param.*", "\\1", x))
+common_seeds <- all_seeds[[1]]
+common_seeds <- unique(all_seeds[[1]])
+
+args <- commandArgs(trailingOnly = TRUE)
+args <- as.numeric(args)
+if(length(args) == 0){
+  rda_it = 1
+}else{
+  rda_it = args
+}
+
+##### Read incomplete dataset
+each_seed <- common_seeds[rda_it]
+dir.create("../results/for_paper/7param/variablephi-heating-updatephi", showWarnings = FALSE)
+
+if (file.exists(paste0("../results/for_paper/7param/variablephi-heating-updatephi/Hes1-log-",
+                       each_seed,"-7param-variablephi-heating-updatephi.rda"))){
+  quit(save = "no")
+}
+
+load(paste0("../results/for_paper/7param/variablephi-heating/Hes1-log-",each_seed,"-7param-variablephi-heating.rda"))
+
+# heating with (3, 3, 1) temperature with phi update ------------------------------------
+#' there are 99 sampled X's (3 x 33) and only 33 actual observations.  
+#' Using the logic about cooling, the default temperatures for Hes1 should be (3, 3, 1).
+config$priorTemperature <- 3
+config$priorTemperatureObs <- 1
+
+xInit <- apply(gpode$xsampled, 2:3, mean)
+thetaInit <- colMeans(gpode$theta)
+phiNoTemperOptimized <- phiUsed
+
+phiNewInit <- gpds:::solveGpdsRcpp(
+  yFull = xInit,
+  odeModel = hes1logmodel,
+  tvecFull = xsim$time,
+  sigmaExogenous = pram.true$sigma,
+  phiExogenous = matrix(numeric(0)),
+  xInitExogenous = xInit,
+  thetaInitExogenous = thetaInit,
+  muExogenous = matrix(numeric(0)),
+  dotmuExogenous = matrix(numeric(0)),
+  priorTemperatureLevel = config$priorTemperature,
+  priorTemperatureDeriv = config$priorTemperature,
+  priorTemperatureObs = config$priorTemperatureObs,
+  kernel = config$kernel,
+  nstepsHmc = config$hmcSteps,
+  burninRatioHmc = config$burninRatio,
+  niterHmc = 1,
+  stepSizeFactorHmc = config$stepSizeFactor,
+  nEpoch = config$max.epoch,
+  bandSize = config$bandsize,
+  useFrequencyBasedPrior = config$useFrequencyBasedPrior,
+  useBand = config$useBand,
+  useMean = config$useMean,
+  useScalerSigma = config$useScalerSigma,
+  useFixedSigma = config$useFixedSigma,
+  verbose = TRUE)
+
+samplesCpp <- gpds:::solveGpdsRcpp(
+  yFull = data.matrix(xsim[,-1]),
+  odeModel = hes1logmodel,
+  tvecFull = xsim$time,
+  sigmaExogenous = pram.true$sigma,
+  phiExogenous = phiNewInit$phi,
+  xInitExogenous = xInit,
+  thetaInitExogenous = thetaInit,
+  muExogenous = matrix(numeric(0)),
+  dotmuExogenous = matrix(numeric(0)),
+  priorTemperatureLevel = config$priorTemperature,
+  priorTemperatureDeriv = config$priorTemperature,
+  priorTemperatureObs = config$priorTemperatureObs,
+  kernel = config$kernel,
+  nstepsHmc = config$hmcSteps,
+  burninRatioHmc = config$burninRatio,
+  niterHmc = config$n.iter,
+  stepSizeFactorHmc = config$stepSizeFactor,
+  nEpoch = config$max.epoch,
+  bandSize = config$bandsize,
+  useFrequencyBasedPrior = config$useFrequencyBasedPrior,
+  useBand = config$useBand,
+  useMean = config$useMean,
+  useScalerSigma = config$useScalerSigma,
+  useFixedSigma = config$useFixedSigma,
+  verbose = TRUE)
+
+phiUsed <- samplesCpp$phi
+samplesCpp <- samplesCpp$llikxthetasigmaSamples
+
+samplesCpp <- samplesCpp[,,1]
+out <- samplesCpp[-1,1,drop=FALSE]
+xCpp <- matrix(out[1:length(data.matrix(xsim[,-1])), 1], ncol=ncol(xsim[,-1]))
+thetaCpp <- out[(length(xCpp)+1):(length(xCpp) + length(hes1logmodel$thetaLowerBound)), 1]
+sigmaCpp <- tail(out[, 1], ncol(xsim[,-1]))
+
+
+llikId <- 1
+xId <- (max(llikId)+1):(max(llikId)+length(data.matrix(xsim[,-1])))
+thetaId <- (max(xId)+1):(max(xId)+length(hes1logmodel$thetaLowerBound))
+sigmaId <- (max(thetaId)+1):(max(thetaId)+ncol(xsim[,-1]))
+
+
+burnin <- as.integer(config$n.iter*config$burninRatio)
+gpode <- list(theta=t(samplesCpp[thetaId, -(1:burnin)]),
+              xsampled=array(t(samplesCpp[xId, -(1:burnin)]),
+                             dim=c(config$n.iter-burnin, nrow(xsim), ncol(xsim)-1)),
+              lglik=samplesCpp[llikId,-(1:burnin)],
+              sigma = t(samplesCpp[sigmaId, -(1:burnin), drop=FALSE]))
+gpode$fode <- sapply(1:length(gpode$lglik), function(t) 
+  with(gpode, gpds:::hes1logmodelODE(theta[t,], xsampled[t,,])), simplify = "array")
+gpode$fode <- aperm(gpode$fode, c(3,1,2))
+
+dotxtrue = gpds:::hes1logmodelODE(pram.true$theta, data.matrix(xtrue[,-1]))
+
+odemodel <- list(times=times, modelODE=modelODE, xtrue=xtrue)
+
+outDir <- "../results/for_paper/7param/variablephi-heating-updatephi/"
+system(paste("mkdir -p", outDir))
+
+for(j in 1:(ncol(xsim)-1)){
+  config[[paste0("phiD", j)]] <- paste(round(phiUsed[,j], 2), collapse = "; ")
+}
+
+gpds:::plotPostSamplesFlex(
+  paste0(outDir, config$modelName,"-",config$seed,"-7param-variablephi-heating-updatephi.pdf"), 
+  xtrue, dotxtrue, xsim, gpode, pram.true, config, odemodel)
+
+save.image(paste0(outDir, config$modelName,"-",config$seed,"-7param-variablephi-heating-updatephi.rda"))
