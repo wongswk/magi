1160dc724386cd140e01179fb084484ec3758e97
Shihao Yang <shihaoyang@MBP2015>
2020-08-26 19:22:31 -0400

    fn plot update

--- R/fn-plot.R
+++ R/fn-plot.R
@@ -45,7 +45,7 @@ ourUB <- apply(desolveOurs, c(1,2), function(x) quantile(x, 0.975))
 times <- ours[[1]]$xdesolveTRUE[,1]
 
 pdf(width = 20, height = 5, file=paste0(outDirWenk, "plotOurs.pdf"))
-layout(t(1:3), widths = c(2,2,1))
+layout(t(1:2))
 for (i in 1:(ncol(xsim)-1)) {
   plot(times, ours[[1]]$xdesolveTRUE[,1+i], type="n", xlab="time", ylab=compnames[i], ylim=c(ylim_lower[i], ylim_upper[i]))
   polygon(c(times, rev(times)), c(ourUB[,i], rev(ourLB[,i])),
@@ -59,12 +59,6 @@ for (i in 1:(ncol(xsim)-1)) {
   }
   
 }
-par(mar=rep(0,4))
-plot(c(0,1), c(0,1) ,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
-legend("center", c("truth", "median of all reconstructed trajectories", "95% interval from the 2.5 and 97.5 \n percentile of all reconstructed trajectories"), lty=c(1,1,0), lwd=c(4,3,0),
-       col = c("red", "black", NA), fill=c(0, 0, "grey80"), pch=c(NA, NA, 15), x.intersp=c(2.5,2.5,0),
-       border=c(0, 0, "grey80"), bty = "n", cex=1.8)
-text(0.1, 0.9, "Ours", cex=2, pos=4)
 dev.off()
 
 
@@ -101,7 +95,7 @@ WenkUB <- apply(desolveWenk, c(1,2), function(x) quantile(x, 0.975))
 times <- Wenk[[1]]$xdesolveTRUE[,1]
 
 pdf(width = 20, height = 5, file=paste0(outDirWenk, "plotWenk.pdf"))
-layout(t(1:3), widths = c(2,2,1))
+layout(t(1:2))
 for (i in 1:(ncol(xsim)-1)) {
   plot(times, Wenk[[1]]$xdesolveTRUE[,1+i], type="n", xlab="time", ylab=compnames[i], ylim=c(ylim_lower[i], ylim_upper[i]))
   polygon(c(times, rev(times)), c(WenkUB[,i], rev(WenkLB[,i])),
@@ -115,14 +109,17 @@ for (i in 1:(ncol(xsim)-1)) {
   }
   
 }
-par(mar=rep(0,4))
-plot(c(0,1), c(0,1) ,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
-legend("center", c("truth", "median of all reconstructed trajectories", "95% interval from the 2.5 and 97.5 \n percentile of all reconstructed trajectories"), lty=c(1,1,0), lwd=c(4,3,0),
-       col = c("red", "black", NA), fill=c(0, 0, "grey80"), pch=c(NA, NA, 15), x.intersp=c(2.5,2.5,0),
-       border=c(0, 0, "grey80"), bty = "n", cex=1.8)
-text(0.1, 0.9, "Wenk", cex=2, pos=4)
 
 dev.off()
+
+pdf(width = 20, height = 1.2, file=paste0(outDirWenk, "legendGrey.pdf"))
+par(mar=rep(0,4))
+plot(c(0,1), c(0,1) ,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
+legend("center", c("truth", "median of all reconstructed trajectories", "95% interval from the 2.5 and 97.5 percentile of all reconstructed trajectories"), lty=c(1,1,0), lwd=c(4,3,0),
+       col = c("red", "black", NA), fill=c(0, 0, "grey80"), pch=c(NA, NA, 15), x.intersp=c(2.5,2.5,0),
+       border=c(0, 0, "grey80"), bty = "n", cex=1.8)
+dev.off()
+
 
 desolveDondel <- sapply(Dondel, function(x) x$xdesolvePM[,-1], simplify = "array")
 DondelLB <- apply(desolveDondel, c(1,2), function(x) quantile(x, 0.025))
@@ -130,7 +127,7 @@ DondelMed <- apply(desolveDondel, c(1,2), function(x) quantile(x, 0.5))
 DondelUB <- apply(desolveDondel, c(1,2), function(x) quantile(x, 0.975))
 times <- Dondel[[1]]$xdesolveTRUE[,1]
 pdf(width = 20, height = 5, file=paste0(outDirWenk, "plotDondel.pdf"))
-layout(t(1:3), widths = c(2,2,1))
+layout(t(1:2))
 for (i in 1:(ncol(xsim)-1)) {
   plot(times, Dondel[[1]]$xdesolveTRUE[,1+i], type="n", xlab="time", ylab=compnames[i], ylim=c(ylim_lower[i], ylim_upper[i]))
   polygon(c(times, rev(times)), c(DondelUB[,i], rev(DondelLB[,i])),
@@ -144,13 +141,6 @@ for (i in 1:(ncol(xsim)-1)) {
   }
   
 }
-par(mar=rep(0,4))
-plot(c(0,1), c(0,1) ,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
-legend("center", c("truth", "median of all reconstructed trajectories", "95% interval from the 2.5 and 97.5 \n percentile of all reconstructed trajectories"), lty=c(1,1,0), lwd=c(4,3,0),
-       col = c("red", "black", NA), fill=c(0, 0, "grey80"), pch=c(NA, NA, 15), x.intersp=c(2.5,2.5,0),
-       border=c(0, 0, "grey80"), bty = "n", cex=1.8)
-text(0.1, 0.9, "Dondelinger", cex=2, pos=4)
-
 dev.off()
 
 
@@ -242,7 +232,7 @@ DondelPostX <- sapply(DondelPostX, identity, simplify = "array")
 
 xdesolveTRUE <- deSolve::ode(y = pram.true$x0, times = xsim$time, func = odemodel$modelODE, parms = pram.true$theta)
 pdf(width = 20, height = 5, file=paste0(outDirWenk, "posteriorxOurs.pdf"))
-layout(t(1:3), widths = c(2,2,1))
+layout(t(1:2))
 for (i in 1:(ncol(xsim)-1)) {
   ourEst <- apply(oursPostX[,i,], 1, quantile, probs = 0.5)
   ourUB <- apply(oursPostX[,i,], 1, quantile, probs = 0.025)
@@ -256,16 +246,11 @@ for (i in 1:(ncol(xsim)-1)) {
   lines(times, xdesolveTRUE[,1+i], col="red", lwd=4)
   lines(times, ourEst, col="forestgreen", lwd=3)
 }
-par(mar=rep(0,4))
-plot(1,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
-legend("center", c("truth", "median posterior mean", "95% interval on posterior mean"), lty=c(1,1,0), lwd=c(4,3,0),
-       col = c("red", "forestgreen", NA), fill=c(0, 0, "skyblue"), pch=c(NA, NA, 15),
-       border=c(0, 0, "skyblue"), bty = "n", cex=1.8)
 dev.off()
 
 pdf(width = 20, height = 5, file=paste0(outDirWenk, "posteriorxOursHoriz.pdf"))
 
-layout(t(1:3), widths = c(2,2,1))
+layout(t(1:2))
 i = 1
 ourEst <- apply(oursPostX[,i,], 1, quantile, probs = 0.5)
 ourUB <- apply(oursPostX[,i,], 1, quantile, probs = 0.025)
@@ -293,21 +278,13 @@ polygon(c(times, rev(times)), c(ourUB, rev(ourLB)),
         col = "skyblue", border = NA)
 lines(times, xdesolveTRUE[,1+i], col="red", lwd=4)
 lines(times, ourEst, col="forestgreen", lwd=3)
-
-
-par(mar=rep(0,4))
-plot(c(0,1), c(0,1) ,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
-legend("center", c("truth", "median of all inferred trajectories", "95% interval from the 2.5 and 97.5 \n percentile of all inferred trajectories"), lty=c(1,1,0), lwd=c(4,3,0),
-       col = c("red", "forestgreen", NA), fill=c(0, 0, "skyblue"), pch=c(NA, NA, 15), x.intersp=c(2.5,2.5,0),
-       border=c(0, 0, "skyblue"), bty = "n", cex=1.8)
-text(0.1, 0.9, "Ours", cex=2, pos=4)
 dev.off()
 
 
 
 xdesolveTRUE <- deSolve::ode(y = pram.true$x0, times = xsim$time, func = odemodel$modelODE, parms = pram.true$theta)
 pdf(width = 20, height = 5, file=paste0(outDirWenk, "posteriorxWenk.pdf"))
-layout(t(1:3), widths = c(2,2,1))
+layout(t(1:2))
 for (i in 1:(ncol(xsim)-1)) {
   ourEst <- apply(WenkPostX[,i,], 1, quantile, probs = 0.5)
   ourUB <- apply(WenkPostX[,i,], 1, quantile, probs = 0.025)
@@ -325,18 +302,12 @@ for (i in 1:(ncol(xsim)-1)) {
     mtext("R", cex=2)
   }
 }
-par(mar=rep(0,4))
-plot(c(0,1), c(0,1) ,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
-legend("center", c("truth", "median of all inferred trajectories", "95% interval from the 2.5 and 97.5 \n percentile of all inferred trajectories"), lty=c(1,1,0), lwd=c(4,3,0),
-       col = c("red", "forestgreen", NA), fill=c(0, 0, "skyblue"), pch=c(NA, NA, 15), x.intersp=c(2.5,2.5,0),
-       border=c(0, 0, "skyblue"), bty = "n", cex=1.8)
-text(0.1, 0.9, "Wenk", cex=2, pos=4)
 dev.off()
 # sink()
 
 xdesolveTRUE <- deSolve::ode(y = pram.true$x0, times = xsim$time, func = odemodel$modelODE, parms = pram.true$theta)
 pdf(width = 20, height = 5, file=paste0(outDirWenk, "posteriorxDondel.pdf"))
-layout(t(1:3), widths = c(2,2,1))
+layout(t(1:2))
 for (i in 1:(ncol(xsim)-1)) {
   ourEst <- apply(DondelPostX[,i,], 1, quantile, probs = 0.5)
   ourUB <- apply(DondelPostX[,i,], 1, quantile, probs = 0.025)
@@ -354,10 +325,25 @@ for (i in 1:(ncol(xsim)-1)) {
     mtext("R", cex=2)
   }
 }
+dev.off()
+
+pdf(width = 20, height = 1.2, file=paste0(outDirWenk, "legendBlue.pdf"))
 par(mar=rep(0,4))
 plot(c(0,1), c(0,1) ,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
-legend("center", c("truth", "median of all inferred trajectories", "95% interval from the 2.5 and 97.5 \n percentile of all inferred trajectories"), lty=c(1,1,0), lwd=c(4,3,0),
+legend("center", c("truth", "median of all inferred trajectories", "95% interval from the 2.5 and 97.5 percentile of all inferred trajectories"), lty=c(1,1,0), lwd=c(4,3,0),
        col = c("red", "forestgreen", NA), fill=c(0, 0, "skyblue"), pch=c(NA, NA, 15), x.intersp=c(2.5,2.5,0),
        border=c(0, 0, "skyblue"), bty = "n", cex=1.8)
-text(0.1, 0.9, "Dondelinger", cex=2, pos=4)
+dev.off()
+
+
+pdf(width = 20, height = 0.5, file=paste0(outDirWenk, "header.pdf"))
+par(mar=rep(0,4))
+plot(c(0,1), c(0,1) ,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
+text(0.5, 0.5, "Ours", cex=2, font=2)
+plot(c(0,1), c(0,1) ,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
+text(0.5, 0.5, "Wenk", cex=2, font=2)
+plot(c(0,1), c(0,1) ,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
+text(0.5, 0.5, "Dondelinger", cex=2, font=2)
+plot(c(0,1), c(0,1) ,type='n', xaxt='n', yaxt='n', xlab=NA, ylab=NA, frame.plot = FALSE)
+text(0.5, 0.5, "Ramsay", cex=2, font=2)
 dev.off()
