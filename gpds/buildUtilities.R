f7d65abfdf5e612e934c554f66aa822898a86f93
Shihao Yang <shihaoyang@MBP2015>
2020-09-01 15:05:18 -0400

    remove folders that no longer use

--- gpds/buildUtilities.R
+++
@@ -1,3 +0,0 @@
-cat("test finished, now running test coverage report...\n")
-covr::report(covr::package_coverage("."))
-cat("test coverage report generated.")
--- gpds/tests/testthat.R
+++
@@ -1,9 +0,0 @@
-library(testthat)
-
-testthat::test_that("on attach messages", {
-  expect_message(library(gpds))
-})
-
-test_check("gpds")
-
-#' check test code coverage
