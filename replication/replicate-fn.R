307af4430c4444081a7fd15312daf489eb60c700
Shihao Yang <shihaoyang@tr3970x>
2020-09-10 02:08:36 +0800

    replicate

--- replication/replicate-fn.R
+++ replication/replicate-fn.R
@@ -1,7 +1,7 @@
 ## Run this script to reproduce FitzHugh-Nagumo (FN) system results in paper, for the same 100 random seeds
 
 ## Perform inference for the 100 seeds (can be run in parallel)
-for (i in 1:100) {
+for (i in 1:112) {
   system(paste0("Rscript run-fn.R ", i))
 }
 
