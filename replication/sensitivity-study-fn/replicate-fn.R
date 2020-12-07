43306097bceee9053614d6230a8fd308c48f36df
Shihao Yang <shihaoyang@MBP2015>
2020-12-07 00:53:27 -0500

    running more experiments

--- replication/sensitivity-study-fn/replicate-fn.R
+++ replication/sensitivity-study-fn/replicate-fn.R
@@ -2,8 +2,36 @@
 
 ## Perform inference for the 100 seeds (can be run in parallel)
 for (i in 1:100) {
-  system(paste0("Rscript run-fn.R 2 ", i, " 21"), wait=FALSE)
+  system(paste0("Rscript run-fn-noband-fixphi.R 5 ", i, " 6 cooling withmeanBand"), wait=FALSE)
 }
+summarize("../results/fn-fill5-nobs6-withmeanBand-fixphi-cooling/")
+
+
+for (i in 1:100) {
+  system(paste0("Rscript run-fn-noband-fixphi.R 5 ", i, " 6 heating withmeanBand"), wait=FALSE)
+}
+summarize("../results/fn-fill5-nobs6-withmeanBand-fixphi-heating/")
+
+
+for (i in 1:100) {
+  system(paste0("Rscript run-fn-noband-fixphi.R 6 ", i, " 6 heating withmeanBand"), wait=FALSE)
+}
+summarize("../results/fn-fill6-nobs6-withmeanBand-fixphi-heating/")
+
+for (i in 1:100) {
+  system(paste0("Rscript run-fn-noband-fixphi.R 6 ", i, " 6 cooling withmeanBand"), wait=FALSE)
+}
+summarize("../results/fn-fill6-nobs6-withmeanBand-fixphi-cooling/")
+
+for (i in 1:100) {
+  system(paste0("Rscript run-fn-noband-fixphi.R 7 ", i, " 6 heating withmeanBand"), wait=FALSE)
+}
+summarize("../results/fn-fill7-nobs6-withmeanBand-fixphi-heating/")
+
+for (i in 1:100) {
+  system(paste0("Rscript run-fn-noband-fixphi.R 7 ", i, " 6 cooling withmeanBand"), wait=FALSE)
+}
+summarize("../results/fn-fill7-nobs6-withmeanBand-fixphi-cooling/")
 
 ## Summarize results, make table and figures
 source("fn-summarize.R")
