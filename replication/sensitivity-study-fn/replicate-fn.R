1a86602b3a18aa8b4fed96b6f0cda402b2d1c257
Shihao Yang <shihaoyang@MBP2015>
2021-01-21 22:52:46 -0500

    replication script clean up

--- replication/sensitivity-study-fn/replicate-fn.R
+++ replication/sensitivity-study-fn/replicate-fn.R
@@ -1,10 +1,30 @@
 ## Run this script to reproduce FitzHugh-Nagumo (FN) system results in paper, for the same 100 random seeds
 
 ## Perform inference for the 100 seeds (can be run in parallel)
-for (i in 1:100) {
-  system(paste0("Rscript run-fn.R 2 ", i, " 21"), wait=FALSE)
-}
-
 ## Summarize results, make table and figures
-source("fn-summarize.R")
 
+source("fn-summarize.R")
+for (i in 1:100) {
+  system(paste0("Rscript run-fn.R 4 ", i, " 21"), wait=FALSE)
+}
+summarize("../results/fn-fill4-nobs21/")
+
+for (i in 1:100) {
+  system(paste0("Rscript run-fn.R 0 ", i, " 41"), wait=FALSE)
+}
+summarize("../results/fn-fill0-nobs41/")
+
+for (i in 1:100) {
+  system(paste0("Rscript run-fn.R 1 ", i, " 41"), wait=FALSE)
+}
+summarize("../results/fn-fill1-nobs41/")
+
+for (i in 1:100) {
+  system(paste0("Rscript run-fn.R 2 ", i, " 41"), wait=FALSE)
+}
+summarize("../results/fn-fill2-nobs41/")
+
+for (i in 1:100) {
+  system(paste0("Rscript run-fn.R 3 ", i, " 41"), wait=FALSE)
+}
+summarize("../results/fn-fill3-nobs41/")
