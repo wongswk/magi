c3138b8f4583c655a34acd4b95b08ece8759133c
Shihao Yang <shihaoyang@MBP2015>
2021-03-10 14:14:20 -0500

    more replication script

--- replication/sensitivity-study-fn/replicate-fn.R
+++ replication/sensitivity-study-fn/replicate-fn.R
@@ -4,31 +4,99 @@
 ## Summarize results, make table and figures
 
 source("fn-summarize.R")
-for (i in 1:100) {
-  system(paste0("Rscript run-fn.R 4 ", i, " 21"), wait=FALSE)
-}
+library(parallel)
+mclapply(1:100, function(i){
+  system(paste0("Rscript run-fn.R 4 ", i, " 21"), wait=TRUE)
+}, mc.cores=100)
 summarize("../results/fn-fill4-nobs21/")
 
 for (i in 1:100) {
-  system(paste0("Rscript run-fn.R 0 ", i, " 41"), wait=FALSE)
+  system(paste0("Rscript run-fn.R ", i), wait = TRUE)
 }
+library(parallel)
+mclapply(1:2000, function(i){
+  system("Rscript run-fn.R", wait=TRUE)
+}, mc.cores=120)
+
+
+mclapply(1:100, function(i){
+  system(paste0("Rscript run-fn.R 0 ", i, " 41"), wait=TRUE)
+}, mc.cores=100)
 summarize("../results/fn-fill0-nobs41/")
 
-for (i in 1:100) {
-  system(paste0("Rscript run-fn.R 1 ", i, " 41"), wait=FALSE)
-}
+mclapply(1:100, function(i){
+  system(paste0("Rscript run-fn.R 1 ", i, " 41"), wait=TRUE)
+}, mc.cores=100)
 summarize("../results/fn-fill1-nobs41/")
 
-for (i in 1:100) {
-  system(paste0("Rscript run-fn.R 2 ", i, " 41"), wait=FALSE)
-}
+mclapply(1:100, function(i){
+  system(paste0("Rscript run-fn.R 2 ", i, " 41"), wait=TRUE)
+}, mc.cores=100)
 summarize("../results/fn-fill2-nobs41/")
 
-for (i in 1:100) {
-  system(paste0("Rscript run-fn.R 3 ", i, " 41"), wait=FALSE)
-}
+mclapply(1:100, function(i){
+  system(paste0("Rscript run-fn.R 3 ", i, " 41"), wait=TRUE)
+}, mc.cores=100)
 summarize("../results/fn-fill3-nobs41/")
 
-for (i in 1:100) {
-  system(paste0("python fnmap.py --seed_id ", i), wait=FALSE)
+{
+mclapply(1:100, function(i){
+  system(paste0("Rscript run-fn.R 4 ", i, " 11"), wait=TRUE)
+}, mc.cores=100)
+summarize("../results/fn-fill4-nobs11/")
+
+mclapply(1:100, function(i){
+  system(paste0("Rscript run-fn.R 5 ", i, " 11"), wait=TRUE)
+}, mc.cores=100)
+summarize("../results/fn-fill5-nobs11/")
+
+mclapply(1:100, function(i){
+  system(paste0("Rscript run-fn.R 6 ", i, " 11"), wait=TRUE)
+}, mc.cores=100)
+summarize("../results/fn-fill6-nobs11/")
+
 }
+
+{
+  mclapply(1:100, function(i){
+    system(paste0("Rscript run-fn.R 4 ", i, " 21"), wait=TRUE)
+  }, mc.cores=100)
+  summarize("../results/fn-fill4-nobs21/")
+  
+  mclapply(1:100, function(i){
+    system(paste0("Rscript run-fn.R 5 ", i, " 21"), wait=TRUE)
+  }, mc.cores=100)
+  summarize("../results/fn-fill5-nobs21/")
+  
+  mclapply(1:100, function(i){
+    system(paste0("Rscript run-fn.R 3 ", i, " 21"), wait=TRUE)
+  }, mc.cores=100)
+  summarize("../results/fn-fill3-nobs21/")
+}
+
+x <- mclapply(1:100, function(i){
+  system(paste0("python3 fnmap.py --nobs 6 --fill 5 --seed_id ", i), wait=TRUE)
+}, mc.cores=100)
+
+mclapply(1:100, function(i){
+  system(paste0("Rscript run-fn.R 5 ", i, " 6"), wait=TRUE)
+}, mc.cores=100)
+summarize("../results/fn-fill5-nobs6/")
+
+mclapply(1:100, function(i){
+  system(paste0("Rscript run-fn.R 6 ", i, " 6"), wait=TRUE)
+}, mc.cores=100)
+summarize("../results/fn-fill6-nobs6/")
+
+mclapply(1:100, function(i){
+  system(paste0("Rscript run-fn.R 7 ", i, " 6"), wait=TRUE)
+}, mc.cores=100)
+summarize("../results/fn-fill7-nobs6/")
+
+x <- mclapply(1:100, function(i){
+  system(paste0("python3 mle_euler.py --nobs 6 --fill 5 --seed_id ", i), wait=TRUE)
+}, mc.cores=100)
+
+x <- mclapply(1:100, function(i){
+  system(paste0("python3 mle_fdgm.py --nobs 6 --fill 4 --seed_id ", i), wait=TRUE)
+}, mc.cores=25)
