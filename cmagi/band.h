6570cd21bc2c8f9ffd19c90f8af2e3e73bfcf193
Shihao Yang <shihaoyang@MBP2015>
2021-06-01 16:23:08 -0400

    band matrix test code dummy tvec

--- cmagi/band.h
+++ cmagi/band.h
@@ -17,7 +17,7 @@ extern "C" {
   void xthetallikBandC( const double *xtheta, const double *Vmphi, const double *VKinv, const double *VCinv,
                         const double *Rmphi, const double *RKinv, const double *RCinv, const int *bandsize, const int *nn,
                         const double *sigma, const double *yobs, double *ret, double *retgrad,
-                        const std::function<arma::mat (arma::vec, arma::mat)> & fODE);
+                        const std::function<arma::mat (arma::vec, arma::mat, arma::vec)> & fODE);
   // previous .C wrapper doesn't work with Rcpp auto-generated wrapper
   void bmatvecmult(const double *a, const double *b, const int *bandsize, const int *matdim, double *result);
   void bmatvecmultT(const double *a, const double *b, const int *bandsize, const int *matdim, double *result);
