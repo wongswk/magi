d07e39003794056b656dd9d372d49c9431eb32a3
Shihao Yang <shihaoyang@MBP2015>
2021-06-24 19:14:57 -0400

    move eigen value/vector truncation idea

--- rmagi/R/kernel.R
+++ rmagi/R/kernel.R
@@ -249,43 +249,7 @@ calCovPeriodicWarpMatern <- function(phi, r, signr, complexity=0) {
 }
 
 #' calculate number of eigen values to preserve based on frobenius norm
-#' @export
-truncEigen <- function(eigenValues, frobeniusNormApprox = 0.99){
-  frobeniusNorm <- sum(eigenValues^2)
-  frobeniusNormTrunc <- cumsum(eigenValues^2)
-  min(which(frobeniusNormTrunc/frobeniusNorm > frobeniusNormApprox))
-}
-
-#' truncate gpCov object for low rank approximation
-#' 
-#' the largest 1-over-eigenvalue will be preserved, and the rest will be deleted
-#' for a low-rank representation from spectral decomposition
-#' 
-#' mphi SVD not helping because complexity is 2mn, comparing to original n^2
-#' however we need m to be around n/2 to preserve accuracy due to low decrease d
-#' 
-#' @param cKeep number of eigen values to keep for C matrix
-#' @param kKeep number of eigen values to keep for K matrix
-#' @export
-truncCovByEigen <- function(gpCov, cKeep, kKeep){
-  cKeepId <- (ncol(gpCov$CeigenVec)-cKeep+1):ncol(gpCov$CeigenVec)
-  gpCov$Ceigen1over <- gpCov$Ceigen1over[cKeepId]
-  gpCov$CeigenVec <- gpCov$CeigenVec[,cKeepId]
-
-  kKeepId <- (ncol(gpCov$KeigenVec)-kKeep+1):ncol(gpCov$KeigenVec)  
-  gpCov$Keigen1over <- gpCov$Keigen1over[kKeepId]
-  gpCov$KeigenVec <- gpCov$KeigenVec[,kKeepId]
-  
-  # mKeepId <- 1:mKeep
-  # gpCov$mphiu <- gpCov$mphiu[mKeepId,]
-  # gpCov$mphid <- gpCov$mphid[mKeepId]
-  # gpCov$mphiv <- gpCov$mphiv[mKeepId,]
-  
-  gpCov
-}
-
-#' calculate number of eigen values to preserve based on frobenius norm
-#' @export
+#'
 bandCov <- function(gpCov, bandsize = 20){
   gpCov$CinvBand <- mat2band(gpCov$Cinv, bandsize)
   gpCov$mphiBand <- mat2band(gpCov$mphi, bandsize)
@@ -296,7 +260,6 @@ bandCov <- function(gpCov, bandsize = 20){
 
 #' Converts a matrix to banded form
 #' 
-#' @export
 mat2band <- function(a, bandsize) {
   N <- nrow(a)
   A <- matrix(0,nrow = 2*bandsize+1, ncol = N)
