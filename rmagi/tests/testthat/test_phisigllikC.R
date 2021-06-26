21807ce1eedf602e80bc468def4ebf46a6cb7123
Shihao Yang <shihaoyang@MBP2015>
2021-06-25 23:05:30 -0400

    test

--- rmagi/tests/testthat/test_phisigllikC.R
+++ rmagi/tests/testthat/test_phisigllikC.R
@@ -25,26 +25,26 @@ signr <- -sign(foo)
 
 for(kerneltype in c("compact1","rbf","matern","periodicMatern","generalMatern")){
   testpoint <- abs(rnorm(length(phitrue[[kerneltype]])+1))
-  xc <- phisigllikC( testpoint, data.matrix(fn.sim[,1:2]), r, kerneltype)
-  xr <- phisigllik( testpoint, fn.sim[,1:2], r, signr, TRUE, kerneltype)
+  xc <- magi:::phisigllikC( testpoint, data.matrix(fn.sim[,1:2]), r, kerneltype)
+  xr <- magi:::phisigllik( testpoint, fn.sim[,1:2], r, signr, TRUE, kerneltype)
   
-  test_that(paste("phisigllik value -", kerneltype), {
+  test_that(paste("magi:::phisigllik value -", kerneltype), {
     expect_equal(xc$value, as.numeric(xr), tolerance = 1e-4)
     expect_equal(as.numeric(xc$grad), attr(xr, "grad"), tolerance = 1e-4)
   })
   
-  test_that(paste("phisigllik gradient -", kerneltype), {
+  test_that(paste("magi:::phisigllik gradient -", kerneltype), {
     if(kerneltype=="periodicMatern"){
-      skip("periodicMatern phisigllik gradient has problem for eta, needs debug")
+      skip("periodicMatern magi:::phisigllik gradient has problem for eta, needs debug")
     }
     x0 <- c(phitrue[[kerneltype]]*exp(rnorm(length(phitrue[[kerneltype]]))/10), noise)
-    gradTrue <- phisigllikC(x0, data.matrix(fn.sim[,1:2]), r, kerneltype)$grad
+    gradTrue <- magi:::phisigllikC(x0, data.matrix(fn.sim[,1:2]), r, kerneltype)$grad
     gradNum <- c()
     for(i in 1:(length(phitrue[[kerneltype]])+1)){
       x1 = x0
       x1[i] = x1[i] + 1e-9
-      gradNum[i] <- ((phisigllikC(x1, data.matrix(fn.sim[,1:2]), r, kerneltype)$value - 
-                        phisigllikC(x0, data.matrix(fn.sim[,1:2]), r, kerneltype)$value)/1e-9)
+      gradNum[i] <- ((magi:::phisigllikC(x1, data.matrix(fn.sim[,1:2]), r, kerneltype)$value -
+                        magi:::phisigllikC(x0, data.matrix(fn.sim[,1:2]), r, kerneltype)$value)/1e-9)
     }
     expect_equal(gradNum, as.numeric(gradTrue), tolerance = 1e-3)
   })
@@ -53,14 +53,14 @@ for(kerneltype in c("compact1","rbf","matern","periodicMatern","generalMatern"))
     if(kerneltype=="periodicMatern"){
       skip("periodicMatern phisigSample runs with error, needs debug")
     }
-    phisigSample(data.matrix(fn.sim[,1:2]), r, c(phitrue[[kerneltype]], noise),
+    magi:::phisigSample(data.matrix(fn.sim[,1:2]), r, c(phitrue[[kerneltype]], noise),
                  rep(0.03,5), 20, F, kerneltype)
   })
 }
 
 for(kerneltype in c("compact1","rbf","matern")){
   testpoint <- abs(rnorm(length(phitrue[[kerneltype]])+1))
-  xc <- phisigllikC( testpoint, data.matrix(fn.sim[,1:2]), r, kerneltype)
+  xc <- magi:::phisigllikC( testpoint, data.matrix(fn.sim[,1:2]), r, kerneltype)
   xc2 <- magi:::phisigllikHard2DC( testpoint, data.matrix(fn.sim[,1:2]), r, kerneltype)
   
   test_that(paste("phisigllik returns -", kerneltype), {
