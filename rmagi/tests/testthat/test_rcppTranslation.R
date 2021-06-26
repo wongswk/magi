c2cacf3da8aebf00834d8518f58c6cf626a6d731
Shihao Yang <shihaoyang@MBP2015>
2021-06-25 23:09:02 -0400

    test

--- rmagi/tests/testthat/test_rcppTranslation.R
+++ rmagi/tests/testthat/test_rcppTranslation.R
@@ -1,11 +1,11 @@
 library(magi)
 
 test_that("output custom object gpcov", {
-    out = maternCovTestOutput(1:2, cbind(1:2, 2:1))
+    out = magi:::maternCovTestOutput(1:2, cbind(1:2, 2:1))
     expect_equal(out$C, cbind(c(0.8286492424, 0.523994108), c(0.5239941088, 0.82864924)))
 })
 
-input_cov = maternCovTestOutput(1:2, cbind(1:2, 2:1))
+input_cov = magi:::maternCovTestOutput(1:2, cbind(1:2, 2:1))
 input_cov$Cinv = solve(input_cov$C)
 input_cov$mphi = input_cov$C + 1
 input_cov$Kinv = input_cov$C + 2
@@ -17,5 +17,5 @@ input_cov$dotmu = c(21, 22)
 input_cov$bandsize = 2
 
 test_that("input custom object gpcov", {
-    expect_equal(maternCovTestInput(input_cov), input_cov$Cinv)
+    expect_equal(magi:::maternCovTestInput(input_cov), input_cov$Cinv)
 })
