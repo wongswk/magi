ce3d105af097e4f6fc71ab57e71b153c614ae3dc
Shihao Yang <shihaoyang@MBP2015>
2021-07-16 13:58:49 -0400

    clean up verbose

--- cmagi/Sampler.cpp
+++ cmagi/Sampler.cpp
@@ -40,9 +40,8 @@ void Sampler::sampleChian(const arma::vec &xthetasigmaInit, const arma::vec &ste
 
         if (verbose && (t % 100 == 1)){
             std::cout << "t = " << t << "; acceptance rate = " << acceptRate << "; theta = "
-                      << xth.submat(arma::span(yobs.size(), yobs.size() + model.thetaSize - 1), arma::span(t, t)).t();
-            std::cout << "t = " << t << "; stepRandom = " << stepRandom.subvec(0, 4).t();  // confirmed with seed setting
-            std::cout << "t = " << t << "; hmcpostsample.lprvalue = " << hmcpostsample.lprvalue << "\n";
+                      << xth.submat(arma::span(yobs.size(), yobs.size() + model.thetaSize - 1), arma::span(t, t)).t()
+            << "; log-posterior value = " << hmcpostsample.lprvalue << "\n";
         }
     }
 }
