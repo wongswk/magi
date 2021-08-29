06b1fdddfef55f41d8e9ddf698e37d69e3081f40
Shihao Yang <shihaoyang@MBP2015>
2021-08-28 23:10:23 -0400

    strip

--- cmagi/gpsmoothing.h
+++ cmagi/gpsmoothing.h
@@ -4,50 +4,12 @@
 
 #ifndef MAGI_MULTI_LANG_GPSMOOTHING_H
 #define MAGI_MULTI_LANG_GPSMOOTHING_H
-#define BOOST_DISABLE_ASSERTS
 #define EIGEN_NO_DEBUG
-
-#include "classDefinition.h"
-arma::vec optimizeThetaInit(const arma::mat & yobsInput,
-                            const OdeSystem & fOdeModelInput,
-                            const std::vector<gpcov> & covAllDimensionsInput,
-                            const arma::vec & sigmaAllDimensionsInput,
-                            const arma::vec & priorTemperatureInput,
-                            const arma::mat & xInitInput,
-                            const bool useBandInput);
 
 arma::vec gpsmooth(const arma::mat & yobsInput,
                    const arma::mat & distInput,
                    std::string kernelInput,
                    const double sigmaExogenScalar = -1,
                    bool useFrequencyBasedPrior = false);
-
-arma::cube calcMeanCurve(const arma::vec & xInput,
-                         const arma::vec & yInput,
-                         const arma::vec & xOutput,
-                         const arma::mat & phiCandidates,
-                         const arma::vec & sigmaCandidates,
-                         const std::string kerneltype = "generalMatern",
-                         const bool useDeriv = false);
-
-arma::mat optimizePhi(const arma::mat & yobsInput,
-                      const arma::vec & tvecInput,
-                      const OdeSystem & fOdeModelInput,
-                      const arma::vec & sigmaAllDimensionsInput,
-                      const arma::vec & priorTemperatureInput,
-                      const arma::mat & xInitInput,
-                      const arma::vec & thetaInitInput,
-                      const arma::mat & phiInitInput,
-                      const arma::uvec & missingComponentDim);
-
-arma::mat optimizeXmissingThetaPhi(const arma::mat & yobsInput,
-                                   const arma::vec & tvecInput,
-                                   const OdeSystem & fOdeModelInput,
-                                   const arma::vec & sigmaAllDimensionsInput,
-                                   const arma::vec & priorTemperatureInput,
-                                   const arma::mat & xInitInput,
-                                   const arma::vec & thetaInitInput,
-                                   const arma::mat & phiInitInput,
-                                   const arma::uvec & missingComponentDim);
 
 #endif //MAGI_MULTI_LANG_GPSMOOTHING_H
--- rmagi/inst/include/magi.h
+++ rmagi/inst/include/magi.h
@@ -1,6 +1,5 @@
 #ifndef RCPP_MAGI_H
 #define RCPP_MAGI_H
 
-#include "../../src/RcppTranslation.h"
 
 #endif // RCPP_MAGI_H
