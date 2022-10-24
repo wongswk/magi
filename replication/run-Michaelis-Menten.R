4da56b179839bca112d344cac4e10d37aca1e5da
Shihao Yang <shihaoyang@MBP2015>
2022-10-24 14:30:39 -0400

    ready for repeated experiments for MM

---
+++ replication/run-Michaelis-Menten.R
@@ -0,0 +1,147 @@
+library(magi)
+
+outDir <- "../results/Michaelis-Menten/"
+dir.create(outDir, showWarnings = FALSE, recursive = TRUE)
+realdata <- read.csv(paste0(outDir, "hydrolysis.csv"))
+matplot(realdata$t, realdata[,-1], type="b")
+
+args <- commandArgs(trailingOnly = TRUE)
+args <- as.numeric(args)
+if(length(args) > 0){
+  seed <- as.numeric(args[1])
+  linfillspace = as.numeric(args[2])
+  if(args[3] == "fullobs"){
+    noise = c(0.002, 0.02, 0.002, 0.02)
+  }else{
+    noise = c(NA, 0.02, NA, 0.02)
+  }
+}else{
+  seed <- 123
+  linfillspace <- 0.5
+  noise = c(NA, 0.02, NA, 0.02)
+}
+
+# set up configuration if not already exist ------------------------------------
+# if(!exists("config")){
+config <- list(
+  nobs = nrow(realdata),
+  noise = noise,  # noise = c(0.01, 0.01, 0.01, 0.01), for fully observed case
+  kernel = "generalMatern",
+  seed = seed,
+  bandsize = 20,
+  hmcSteps = 100,
+  n.iter = 20001,
+  stepSizeFactor = 0.01,
+  linfillspace = linfillspace, 
+  t.end = 70,
+  modelName = "Michaelis-Menten"
+)
+# }
+
+
+# initialize global parameters, true x, simulated x ----------------------------
+# parameters and initial conditions that seem to mimic the real data well
+pram.true <- list( 
+  theta=c(0.9, 0.75, 2.54),
+  x0 = c(0.1, 1, 0, 0),
+  phi = cbind(c(0.1, 70), c(1, 30), c(0.1, 70), c(1, 30)),
+  sigma=config$noise
+)
+
+times <- seq(0,config$t.end,length=1001)
+
+modelODE <- function(t, state, parameters) {
+  list(as.vector(magi:::MichaelisMentenModelODE(parameters, t(state), t)))
+}
+
+xtrue <- deSolve::ode(y = pram.true$x0, times = times, func = modelODE, parms = pram.true$theta)
+xtrue <- data.frame(xtrue)
+matplot(xtrue[, "time"], xtrue[, c(3,5)], type="l", lty=1)
+
+xtrueFunc <- lapply(2:ncol(xtrue), function(j)
+  approxfun(xtrue[, "time"], xtrue[, j]))
+
+xsim <- data.frame(time = round(realdata$t / config$linfillspace) * config$linfillspace)
+xsim <- cbind(xsim, sapply(xtrueFunc, function(f) f(xsim$time)))
+xtest <- xsim
+
+set.seed(config$seed)
+for(j in 1:(ncol(xsim)-1)){
+  xsim[,1+j] <- xsim[,1+j]+rnorm(nrow(xsim), sd=config$noise[j])
+}
+
+xsim.obs <- xsim[seq(1,nrow(xsim), length=config$nobs),]
+xsim.obs <- rbind(c(0, pram.true$x0), xsim.obs)
+matplot(xsim.obs$time, xsim.obs[,-1], type="p", col=1:(ncol(xsim)-1), pch=20, add = TRUE)
+
+matplot(xsim.obs$time, xsim.obs[,-1], type="p", col=1:(ncol(xsim)-1), pch=20)
+
+## Linearly interpolate using fixed interval widths
+fillC <- seq(0, config$t.end, by = config$linfillspace)
+xsim <- data.frame(time = fillC)
+xsim <- cbind(xsim, matrix(NaN, nrow = length(fillC), ncol = ncol(xsim.obs)-1 ))
+for (i in 1:length(fillC)) {
+  loc <- match( fillC[i], xsim.obs[, "time"])
+  if (!is.na(loc))
+    xsim[i,2:ncol(xsim)] = xsim.obs[loc,2:ncol(xsim)];
+}
+
+# cpp inference ----------------------------
+dynamicalModelList <- list(
+  fOde=magi:::MichaelisMentenModelODE,
+  fOdeDx=magi:::MichaelisMentenModelDx,
+  fOdeDtheta=magi:::MichaelisMentenModelDtheta,
+  thetaLowerBound=c(0,-100,0),
+  thetaUpperBound=c(Inf,Inf,Inf),
+  name="Michaelis-Menten"
+)
+
+testDynamicalModel(dynamicalModelList$fOde, dynamicalModelList$fOdeDx, dynamicalModelList$fOdeDtheta, "dynamicalModelList",
+                   data.matrix(xtest[,-1]), pram.true$theta, xtest$time)
+
+config$ndis <- config$t.end / config$linfillspace + 1
+
+sigma_fixed <- config$noise
+sigma_fixed[is.na(sigma_fixed)] <- 1e-4
+
+# MAGI off-the-shelf ----
+# sampler with a good phi supplied, no missing component
+# hyper-parameters affect the inference of missing components (especially if initial condition is not known
+
+OursStartTime <- proc.time()[3]
+
+result <- magi::MagiSolver(xsim[,-1], dynamicalModelList, xsim$time, control = 
+                             list(bandsize=config$bandsize, niterHmc=config$n.iter, nstepsHmc=config$hmcSteps, stepSizeFactor = config$stepSizeFactor,
+                                  burninRatio = 0.5, phi = pram.true$phi, sigma=sigma_fixed, discardBurnin=TRUE, useFixedSigma=TRUE,
+                                  skipMissingComponentOptimization=TRUE))
+
+OursTimeUsed <- proc.time()[3] - OursStartTime
+
+
+gpode <- result
+gpode$fode <- sapply(1:length(gpode$lp), function(t)
+  with(gpode, dynamicalModelList$fOde(theta[t,], xsampled[t,,], xsim$time)), simplify = "array")
+gpode$fode <- aperm(gpode$fode, c(3,1,2))
+
+dotxtrue = dynamicalModelList$fOde(pram.true$theta, data.matrix(xtrue[,-1]), xtrue$time)
+
+odemodel <- list(times=times, modelODE=modelODE, xtrue=xtrue)
+
+for(j in 1:(ncol(xsim)-1)){
+  config[[paste0("phiD", j)]] <- paste(round(gpode$phi[,j], 2), collapse = "; ")
+}
+
+gpode$lglik <- gpode$lp
+pram.true$sigma <- sigma_fixed
+gpode$theta <- cbind(gpode$theta, (gpode$theta[,2]+gpode$theta[,3])/gpode$theta[,1])
+pram.true$theta <- c(pram.true$theta, (pram.true$theta[2]+pram.true$theta[3])/pram.true$theta[1])
+
+save.image(paste0(outDir, config$modelName,"-",config$seed,"-fill", sum(config$linfillspace),"-noise", sum(config$noise, na.rm = TRUE), "-phi", sum(pram.true$phi), ".rda"))
+
+magi:::plotPostSamplesFlex(
+  paste0(outDir, config$modelName,"-",config$seed,"-fill", sum(config$linfillspace),"-noise", sum(config$noise, na.rm = TRUE), "-phi", sum(pram.true$phi), ".pdf"),
+  xtrue, dotxtrue, xsim, gpode, pram.true, config, odemodel)
+tail(gpode$theta)
+
+apply(gpode$xsampled[,1,], 2, median)
+apply(gpode$theta, 2, median)
