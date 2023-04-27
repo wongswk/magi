af83c8f14e64f31cc24bfbc16ef18a7f6584eee6
Samuel <samuel.wong@uwaterloo.ca>
2023-04-27 17:11:39 -0400

    R replication script for revision

--- JSS-replication/replication.R
+++ JSS-replication/replication.R
@@ -47,7 +47,8 @@ matplot(y$time, y[,-1], type = "p", col = 1:(ncol(y)-1), pch = 20, add = TRUE)
 legend("topright", compnames, lty = 1, col = c("black", "red", "green"))
 dev.off()
 
-y[, names(y) != "time"] <- log(y[, names(y) != "time"])
+y.tilde <- y
+y.tilde[, names(y.tilde) != "time"] <- log(y.tilde[, names(y.tilde) != "time"])
 
 hes1logmodelODE <- function (theta, x, tvec) {
 	P = exp(x[, 1])
@@ -101,11 +102,11 @@ hes1logmodelDtheta <- function (theta, x, tvec) {
 }
 
 
-yTest <- matrix(runif(nrow(y) * (ncol(y) - 1)),
-                nrow = nrow(y), ncol = ncol(y) - 1)
+yTest <- matrix(runif(nrow(y.tilde) * (ncol(y.tilde) - 1)),
+                nrow = nrow(y.tilde), ncol = ncol(y.tilde) - 1)
 thetaTest <- runif(7)
 testDynamicalModel(hes1logmodelODE, hes1logmodelDx, hes1logmodelDtheta, 
-                   "Hes1 log", yTest, thetaTest, y[, "time"])
+                   "Hes1 log", yTest, thetaTest, y.tilde[, "time"])
 
 hes1logmodel <- list(
   fOde = hes1logmodelODE,
@@ -115,16 +116,11 @@ hes1logmodel <- list(
   thetaUpperBound = rep(Inf, 7)
 )
 
-hes1result <- MagiSolver(y, hes1logmodel, 
-                         control = list(sigma = param.true$sigma, useFixedSigma = TRUE))
+hes1result <- MagiSolver(y.tilde, hes1logmodel, control = list(sigma = param.true$sigma, useFixedSigma = TRUE))
 
 pdf(file = "figures/hes1-trace.pdf", width = 8, height = 4)
-par(mfrow = c(2, 4), mar = c(5, 2, 1, 1))
 theta.names <- c("a", "b", "c", "d", "e", "f", "g")
-for (i in 1:7) {
-	plot(hes1result$theta[, i], main = theta.names[i], type = "l", ylab = "")
-}
-plot(hes1result$lp, main = "log-post", type = "l", ylab = "")
+plot(hes1result, type = "trace", par.names = theta.names, nplotcol = 4)
 dev.off()
 
 summary(hes1result, par.names = theta.names)
@@ -172,6 +168,68 @@ legend("center", c("truth", "inferred trajectory",
 dev.off()
 
 
+# Figure 2 (Example visualization of manifold constraint)
+set.seed(1234)
+dat <- data.frame(time = c(1, 3, 4, 5), y = c(-2, -1, 1, 1))
+
+tvec <- seq(0, 10, by = 0.05)
+phi <- matrix(c(10, 3))
+sigma <- 0.5
+
+## Conditional mean and covariance given observations, sample 5 trajectories
+condMean <- gpmean(dat$y, dat$time, tvec, phi, sigma)
+condSigma <- gpcov(dat$y, dat$time, tvec, phi, sigma)
+ysim <- MASS::mvrnorm(5, condMean, condSigma)
+
+## Set up a simple linear ODE system and use MAGI to condition on it
+linearODE <- function(theta, x, tvec) {
+  lindt = array(0, c(nrow(x), ncol(x)))
+  lindt[, 1] = theta[1] * x[,1] + theta[2]
+  lindt
+}
+linearDx <- function(theta, x, tvec) {
+  Dx <- array(0, c(nrow(x), ncol(x), ncol(x)))
+  Dx[, 1, 1] = theta[1]
+  Dx
+}
+linearDtheta <- function(theta, x, tvec) {
+  Dtheta <- array(0, c(nrow(x), length(theta), ncol(x)))
+  Dtheta[, 1, 1] = x[,1]
+  Dtheta[, 2, 1] = 1
+  Dtheta
+}
+
+linearmodel <- list(
+  fOde = linearODE,
+  fOdeDx = linearDx,
+  fOdeDtheta = linearDtheta,
+  thetaLowerBound = rep(-Inf, 2),
+  thetaUpperBound = rep(Inf, 2)
+)
+
+yIn <- data.frame(time=tvec, y = NA)
+yIn[yIn$time %in% dat$time, "y"] <- dat$y
+res <- MagiSolver(yIn, linearmodel, control=list(sigma = sigma, phi = phi, useFixedSigma = TRUE, niterHmc = 5000))
+
+## Panel (a): GP given observations only
+pdf("figures/GP-given-obs.pdf", height = 4.5, width = 5)
+par(mar = c(4, 4, 1.5, 1))
+plot(tvec, ysim[1,], type = 'n', ylim = c(-6.5, 7.5), xlab = "Time", ylab = "x")
+polygon(c(rev(tvec), tvec), c(rev(condMean + 1.96 * sqrt(diag(condSigma))), condMean - 1.96 * sqrt(diag(condSigma))), col = 'grey80', border = NA)
+for (j in 1:5)
+  lines(tvec, ysim[j, ], col = rainbow(12)[j+7])
+points(dat$time, dat$y, pch = 20)
+dev.off()
+
+## Panel (b): GP given observations and manifold constraint
+pdf("figures/GP-given-obs-constraint.pdf", height = 4.5, width = 5)
+plot(res, est = "none", ci.col = "grey80", comp.names = "", ylim = c(-6.5, 7.5), xlab = "Time", ylab = "x", obs = FALSE)
+for (j in 1:5)
+  lines(tvec, res$xsampled[sample(1:length(res$lp), 1), , ], col = rainbow(12)[j+7])
+points(dat$time, dat$y, pch = 20)
+dev.off()
+
+
 # Fitzhugh-Nagumo equations
 
 data("FNdat")
@@ -182,7 +240,6 @@ y_I0 <- setDiscretization(FNdat, by = 0.5)
 y_I1 <- setDiscretization(y_I0, level = 1)
 y_I2 <- setDiscretization(y_I0, level = 2)
 y_I3 <- setDiscretization(y_I0, level = 3)
-
 
 fnmodelODE <- function(theta, x, tvec) {
   V <- x[, 1]
@@ -231,17 +288,34 @@ fnmodel <- list(
   thetaUpperBound = c(Inf, Inf, Inf)
 )
 
+
 FNres0 <- MagiSolver(y_I0, fnmodel, control = list(niterHmc = 10000))
 FNres1 <- MagiSolver(y_I1, fnmodel, control = list(niterHmc = 10000))
 FNres2 <- MagiSolver(y_I2, fnmodel, control = list(niterHmc = 10000))
 FNres3 <- MagiSolver(y_I3, fnmodel, control = list(niterHmc = 10000, nstepsHmc = 1000))
 
 FNpar.names <- c("a", "b", "c", "sigmaV", "sigmaR")
+FNsummary <- lapply(list(FNres0, FNres1, FNres2, FNres3),
+               function(x) summary(x, sigma = TRUE, par.names = FNpar.names))
 
-summary(FNres0, sigma = TRUE, par.names = FNpar.names)
-summary(FNres1, sigma = TRUE, par.names = FNpar.names)
-summary(FNres2, sigma = TRUE, par.names = FNpar.names)
-summary(FNres3, sigma = TRUE, par.names = FNpar.names)
+pdf(file = "figures/FNparam.pdf", width = 10, height = 3)
+layout(rbind(c(1:5), rep(6, 5)), heights = c(5, 0.25))
+for (i in 1:length(FNpar.names)) {
+  par(mar = c(2, 4, 1.5, 1))
+  estCI <- sapply(FNsummary, function(x) x[,i])
+  plot(1:4, xlim = c(0, 5), ylim = c(min(estCI[2, ]), max(estCI[3, ])),
+       xaxt = 'n', xlab = '', ylab = '', type = 'n')
+  segments(1:4, y0 = estCI[2, ], y1 = estCI[3, ], col = 1:4, lwd = 2)
+  mtext(FNpar.names[i])
+  points(1:4, estCI[1, ], col = 1:4, cex = 2)
+}
+
+par(mar = rep(0, 4))
+plot(1, type = 'n', xaxt = 'n', yaxt = 'n',
+     xlab = NA, ylab = NA, frame.plot = FALSE)
+legend("center", c("I0", "I1", "I2", "I3"),
+       col = 1:4, lwd = 4, horiz = TRUE, bty = "n")
+dev.off()
 
 fnmodelODEsolve <- function(tvec, state, parameters) {
   list(as.vector(fnmodelODE(parameters, t(state), tvec)))
@@ -367,24 +441,33 @@ for(j in 1:(ncol(y) - 1)){
   y[, 1+j] <- y[, 1+j] + rnorm(nrow(y), sd = param.true$sigma[j])
 }
 
-compnames <- c("TU", "TI", "V")
-complabels <- c("Concentration", "Concentration", "Load")
-pdf(file = "figures/HIVsim.pdf", width = 8, height = 4)
-par(mfrow = c(1, 3), mar = c(4, 4, 1.5, 1))
-for (i in 1:3) {
-  plot(param.true$times, y[, i+1], xlab = "Time", ylab = complabels[i])
-  mtext(compnames[i])
-  lines(xtrue[, "time"], xtrue[, i+1], col = "red", lwd = 2)
-}
-dev.off()
-
-
 phiEst <- matrix(0, nrow = 2, ncol = ncol(y) - 1)
 sigmaInit <- rep(0, ncol(y) - 1)
-for (j in 1:(ncol(y) - 1)){
+for (j in 1:(ncol(y) - 1)) {
   hyperparam <- gpsmoothing(y[, j+1], y[, "time"])
   phiEst[, j] <- hyperparam$phi
   sigmaInit[j] <- hyperparam$sigma
+}
+
+compnames <- c("TU", "TI", "V")
+complabels <- c("Concentration", "Concentration", "Load")
+
+pdf(file = "figures/HIVsim-fit.pdf", width = 8, height = 4)
+par(mfrow = c(1, 3), mar = c(4, 4, 1.5, 1))
+tOut <- seq(0, 20, by = 0.025)
+for (j in 1:3) {
+  plot(y[, "time"], y[, j+1], type = 'n',
+      xlab = "Time", ylab = complabels[j])
+  mtext(compnames[j])
+  fitMean <- gpmean(y[, j+1], y[, "time"], tOut, phiEst[, j], sigmaInit[j])
+  fitCov <- gpcov(y[, j+1], y[, "time"], tOut, phiEst[, j], sigmaInit[j])
+  gp_UB <- fitMean + 1.96 * sqrt(diag(fitCov))
+  gp_LB <- fitMean - 1.96 * sqrt(diag(fitCov))
+
+  polygon(c(tOut, rev(tOut)), c(gp_UB, rev(gp_LB)),
+          col = "grey80", border = NA)
+  points(y[, "time"], y[, j+1], cex = 0.5)
+  lines(tOut, fitMean, col = "steelblue")
 }
 
 colnames(phiEst) <- compnames
@@ -394,11 +477,24 @@ sigmaInit
 phiEst[, 3] <- c(1e7, 0.5)
 sigmaInit[3] <- 100
 
+j <- 3
+fitMean <- gpmean(y[, j+1], y[, "time"], tOut, phiEst[, j], sigmaInit[j])
+fitCov <- gpcov(y[, j+1], y[, "time"], tOut, phiEst[, j], sigmaInit[j])
+gp_UB <- fitMean + 1.96 * sqrt(diag(fitCov))
+gp_LB <- fitMean - 1.96 * sqrt(diag(fitCov))
+
+polygon(c(tOut, rev(tOut)), c(gp_UB, rev(gp_LB)),
+        col = "grey80", border = NA)
+lines(tOut, fitMean, col = "darkorange")
+
+dev.off()
+
 y_I <- setDiscretization(y, level = 1)
 HIVresult <- MagiSolver(y_I, hivtdmodel,
                   control = list(phi = phiEst, sigma = sigmaInit))
 
-summary(HIVresult, par.names = c("lambda", "rho", "delta", "N", "c"))
+summary(HIVresult, sigma = TRUE, par.names =
+   c("lambda", "rho", "delta", "N", "c", "sigma_TU", "sigma_TI", "sigma_V"))
 
 xMean <- apply(HIVresult$xsampled, c(2, 3), mean)
 xLB <- apply(HIVresult$xsampled, c(2, 3), function(x) quantile(x, 0.025))
@@ -414,6 +510,20 @@ for (i in 1:3) {
   lines(y_I$time, xMean[, i], col = "forestgreen", lwd = 2)
   lines(param.true$times, xtrue[, i+1], col = "red", lwd = 1)
 }
+dev.off()
+
+
+# Hamiltonian Monte Carlo - example of sticky samples with high autocorrelation
+
+data("FNdat")
+set.seed(12321)
+
+y_I0 <- setDiscretization(FNdat, by = 0.5)
+y_I3 <- setDiscretization(y_I0, level = 3)
+FNres3b <- MagiSolver(y_I3, fnmodel, control = list(niterHmc = 10000))
+
+pdf(file = "figures/fn-sticky-trace.pdf", width = 8, height = 4)
+plot(FNres3b, type = "trace", par.names = FNpar.names, sigma = TRUE)
 dev.off()
 
 sessionInfo()
