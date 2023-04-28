3ef40fa00dd6dfe6e04d4ee423f181f3339a72db
Shihao Yang <shihaoyang@MBP2015>
2023-04-28 09:11:22 -0400

    confirm autogradient.R on linux server

--- replication/autogradient.R
+++ replication/autogradient.R
@@ -1,33 +1,27 @@
+library("magi")
+library(torch)
+# > sessionInfo()
+# other attached packages:
+#   [1] torch_0.10.0 magi_1.2.2  
+
 x <- cbind(1:4, 4:1, sin(1:4))
 theta <- c(0.022, 0.3, 0.031, 0.028, 0.5, 20, 0.3)
 tvec <- 1:4
 
-dynamicalModelList <- list(
-  modelODE = magi:::hes1modelODE,
-  modelDtheta = magi:::hes1modelDtheta,
-  modelDx = magi:::hes1modelDx
-)
-with(dynamicalModelList, testDynamicalModel(modelODE, modelDx, modelDtheta, "Hes1 system", x, theta))
-
-
-dtheta_byhand = magi:::hes1modelDtheta(theta, x, tvec)
-dx_byhand = magi:::hes1modelDx(theta, x, tvec)
-
-
-library("magi")
-library(torch)
+dtheta_byhand = magi:::hes1logmodelDtheta(theta, x, tvec)
+dx_byhand = magi:::hes1logmodelDx(theta, x, tvec)
 
 
 # Define the original function using torch operations
-hes1modelODE_torch <- function(theta, x) {
-  P = x[, 1]
-  M = x[, 2]
-  H = x[, 3]
+hes1logmodelODE_torch <- function (theta, x, tvec) {
+  P = exp(x[, 1])
+  M = exp(x[, 2])
+  H = exp(x[, 3])
   
-  PMHdt = torch_empty(dim(x))  # only difference from R implementation
-  PMHdt[, 1] = -theta[1] * P * H + theta[2] * M - theta[3] * P
-  PMHdt[, 2] = -theta[4] * M + theta[5] / (1 + P^2)
-  PMHdt[, 3] = -theta[1] * P * H + theta[6] / (1 + P^2) - theta[7] * H
+  PMHdt <- torch_empty(dim(x))
+  PMHdt[, 1] = -theta[1] * H + theta[2] * M / P - theta[3]
+  PMHdt[, 2] = -theta[4] + theta[5] / (1 + P^2) / M
+  PMHdt[, 3] = -theta[1] * P + theta[6] / (1 + P^2) / H - theta[7]
   
   PMHdt
 }
@@ -57,7 +51,8 @@ ode_autograd <- function(ode_func_torch, theta, x, tvec) {
         x$grad$zero_()
       }
       
-      output[i, j]$backward(keep_graph=TRUE)
+      # use keep_graph=TRUE or retain_graph=TRUE depending on torch version
+      output[i, j]$backward(retain_graph=TRUE)  
       
       ode_dtheta[i, , j] = as_array(theta$grad)
       ode_dx[i, , j] = as_array(x$grad[i,])
@@ -68,7 +63,7 @@ ode_autograd <- function(ode_func_torch, theta, x, tvec) {
 }
 
 
-augograd = ode_autograd(hes1modelODE_torch, theta, x, tvec)
+augograd = ode_autograd(hes1logmodelODE_torch, theta, x, tvec)
 sum(abs(augograd$ode_dtheta - dtheta_byhand))
 sum(abs(augograd$ode_dx - dx_byhand))
 
@@ -115,6 +110,7 @@ y$H <- NaN
 y$P[y$time %in% seq(7.5, 240, by = 15)] <- NaN
 y$M[y$time %in% seq(0, 240, by = 15)] <- NaN
 
+dir.create('figures', showWarnings=FALSE)
 pdf(file = "figures/hes1setup.pdf", width = 6, height = 4)
 compnames <- c("P", "M", "H")
 matplot(x[, "time"], x[, -1], type = "l", lty = 1,
@@ -138,45 +134,22 @@ hes1logmodelODE <- function (theta, x, tvec) {
   PMHdt
 }
 
-
-hes1logmodelODE_torch <- function (theta, x, tvec) {
-  P = exp(x[, 1])
-  M = exp(x[, 2])
-  H = exp(x[, 3])
-  
-  PMHdt <- torch_empty(dim(x))
-  PMHdt[, 1] = -theta[1] * H + theta[2] * M / P - theta[3]
-  PMHdt[, 2] = -theta[4] + theta[5] / (1 + P^2) / M
-  PMHdt[, 3] = -theta[1] * P + theta[6] / (1 + P^2) / H - theta[7]
-  
-  PMHdt
-}
-
-hes1logmodelDx <- function (theta, x, tvec) {
-  result <- ode_autograd(hes1logmodelODE_torch, theta, x, tvec)
-  result$ode_dx
-}
-
-
-hes1logmodelDtheta <- function (theta, x, tvec) {
-  result <- ode_autograd(hes1logmodelODE_torch, theta, x, tvec)
-  result$ode_dtheta
-}
-
-
-yTest <- matrix(runif(nrow(y) * (ncol(y) - 1)),
-                nrow = nrow(y), ncol = ncol(y) - 1)
-thetaTest <- runif(7)
-testDynamicalModel(hes1logmodelODE, hes1logmodelDx, hes1logmodelDtheta, 
-                   "Hes1 log", yTest, thetaTest, y[, "time"])
-
 hes1logmodel <- list(
   fOde = hes1logmodelODE,
-  fOdeDx = hes1logmodelDx,
-  fOdeDtheta = hes1logmodelDtheta,
+  fOdeDx = function(theta, x, tvec) 
+    ode_autograd(hes1logmodelODE_torch, theta, x, tvec)$ode_dx,
+  fOdeDtheta = function(theta, x, tvec) 
+    ode_autograd(hes1logmodelODE_torch, theta, x, tvec)$ode_dtheta,
   thetaLowerBound = rep(0, 7),
   thetaUpperBound = rep(Inf, 7)
 )
 
+yTest <- matrix(runif(nrow(y) * (ncol(y) - 1)),
+                nrow = nrow(y), ncol = ncol(y) - 1)
+thetaTest <- runif(7)
+testDynamicalModel(hes1logmodel$fOde, hes1logmodel$fOdeDx, hes1logmodel$fOdeDtheta, 
+                   "Hes1 log", yTest, thetaTest, y[, "time"])
+
+# linux start 14:49pm
 hes1result <- MagiSolver(y, hes1logmodel, 
-                         control = list(sigma = param.true$sigma, useFixedSigma = TRUE))
+                         control = list(sigma = param.true$sigma, useFixedSigma = TRUE, niterHmc=201))
